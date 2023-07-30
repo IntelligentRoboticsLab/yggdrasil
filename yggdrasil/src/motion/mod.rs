96119c4f14b64cada4547254477905067655223d
jakob <jakob.j.jeep@gmail.com>
2023-07-30 23:43:51 +0200

    continued working on motions

--- yggdrasil/src/motion/mod.rs
+++ yggdrasil/src/motion/mod.rs
@@ -1,4 +1,4 @@
-use color_eyre::Result;
+use color_eyre::{eyre::eyre, Result};
 use nidhogg::types::JointArray;
 use nidhogg::NaoState;
 use serde::Deserialize;
@@ -10,20 +10,18 @@ use std::{path::Path, time::Duration};
 use tyr::prelude::*;
 use tyr::Module;
 
-/// TODO: Implement iterator for JointArray for interpolation
-
 #[serde_as]
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 /// Represents a single robot movement.
 pub struct Movement {
     /// Movement target joint positions.
-    target_positions: JointArray<f32>,
+    pub target_positions: JointArray<f32>,
     /// Movement duration.
     #[serde_as(as = "DurationSecondsWithFrac<f64>")]
-    duration: Duration,
+    pub duration: Duration,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 /// Represents a robot motion that consists of multiple movements.
 pub struct Motion {
     /// Joint starting positions for the motion.
@@ -38,30 +36,49 @@ impl Motion {
     /// # Arguments
     ///
     /// * `path` - the `Path` to the file from which to read the motion.
-    pub fn from_path(path: &Path) -> Motion {
-        let file = File::open(&path)
-            .expect(format!("Could not read motion file {}.", path.display()).as_str());
-        serde_json::from_reader(file)
-            .expect(format!("Could deserialize json {}.", path.display()).as_str())
+    pub fn from_path(path: &Path) -> Result<Motion> {
+        match serde_json::from_reader(File::open(&path)?) {
+            Ok(val) => Ok(val),
+            Err(err) => Err(eyre!("Could deserialize json {}: {}.", path.display(), err)),
+        }
+    }
+
+    /// TODO: implemenation
+    /// Get the frames that surround the current time.
+    ///
+    /// # Arguments
+    ///
+    /// * `motion_duration` - Current duration of the motion.
+    pub fn get_frames(&self, motion_duration: &Duration) -> Option<(Movement, Movement)> {
+        Some((
+            Movement {
+                target_positions: JointArray::<f32>::default(),
+                duration: Duration::default(),
+            },
+            Movement {
+                target_positions: JointArray::<f32>::default(),
+                duration: Duration::default(),
+            },
+        ))
     }
 }
 
 #[derive(PartialEq, Eq, Hash, Debug)]
 /// An enumeration of all possible motions.
 pub enum MotionType {
-    SitDown,
-    StandUp,
+    SitDownFromStand,
+    StandUpFromSit,
 }
 
 /// Manages motions, stores all possible motions and keeps track of information
 /// about the motion that is currently being executed.
 struct MotionManager {
-    /// `MotionType` of the current motion.
-    current_motion: Option<MotionType>,
+    /// Current motion.
+    current_motion: Option<Motion>,
     /// Keeps track of when a motion started.
-    current_motion_starting_time: Option<SystemTime>,
-    /// Needed for checking if a motion can start.
-    in_current_motion_initial_position: Option<bool>,
+    motion_starting_time: Option<SystemTime>,
+    /// Needed for checking if initial position still needs to be reached.
+    started_executing_motion: bool,
     /// Contains the mapping from `MotionTypes` to `Motion`.
     motions: HashMap<MotionType, Motion>,
 }
@@ -71,55 +88,180 @@ impl MotionManager {
     ///
     /// # Arguments
     ///
-    /// * `motions` -  A mapping from motion types to the files where the
-    ///                motions are stored.
-    pub fn new(motions: HashMap<MotionType, &str>) -> Self {
+    /// * `motions` - A mapping from motion types to the files where the
+    ///               motions are stored.
+    pub fn new() -> Self {
         MotionManager {
             current_motion: None,
-            current_motion_starting_time: None,
-            in_current_motion_initial_position: None,
-            motions: motions
-                .into_iter()
-                .map(|(k, v)| (k, Motion::from_path(Path::new(v))))
-                .collect(),
+            motion_starting_time: None,
+            started_executing_motion: false,
+            motions: HashMap::new(),
         }
+    }
+
+    /// Adds a motion to the `MotionManger`.
+    ///
+    /// # Arguments
+    ///
+    /// * `motion_type` - Type of the motion.
+    /// * `motion_file` - Path to the file where the motion movements can be found.
+    pub fn add_motion(mut self, motion_type: MotionType, motion_file: &str) -> Result<Self> {
+        self.motions
+            .insert(motion_type, Motion::from_path(Path::new(motion_file))?);
+        Ok(self)
     }
 
     /// Starts a new motion.
     ///
     /// # Arguments
     ///
-    /// * `motion_type` - the motion to start.
+    /// * `motion_type` - The type of motion to start.
     pub fn start_new_motion(&mut self, motion_type: MotionType) {
-        self.current_motion_starting_time = Some(std::time::SystemTime::now());
-        self.current_motion = Some(motion_type);
-
-        // TODO: check this, lerp to starting position if not there yet.
-        self.in_current_motion_initial_position = Some(true)
+        self.started_executing_motion = false;
+        self.current_motion = self.motions.get(&motion_type).cloned();
     }
 }
 
+/// Checks if the current position has reached the target position with a certain
+/// margin of error.
+///
+/// # Arguments
+///
+/// * `current_positions` - Positions of which you want to check if they have reached a certain
+///                         position.
+/// * `target_positions` - Positions of which you want to check if they ahve been reached.
+/// * `error_margin` - Range within which a target position has been reached.
+fn reached_position(
+    current_positions: &JointArray<f32>,
+    target_positions: &JointArray<f32>,
+    error_margin: f32,
+) -> bool {
+    let curr_iter = current_positions.clone().into_iter();
+    let target_iter = target_positions.clone().into_iter();
+
+    curr_iter
+        .zip(target_iter)
+        .map(|(curr, target)| target - error_margin <= curr && curr <= target + error_margin)
+        .collect::<Vec<bool>>()
+        .contains(&false)
+}
+
+/// Performs linear interpolation between two `JointArray<f32>`.
+///
+/// # Arguments
+///
+/// * `current_positions` - Starting position.
+/// * `target_positions` - Final position.
+/// * `scalar` - Scalar from 0-1 that indicates what weight to assign to each position.
+fn lerp(
+    current_positions: &JointArray<f32>,
+    target_positions: &JointArray<f32>,
+    scalar: f32,
+) -> JointArray<f32> {
+    let curr_iter = current_positions.clone().into_iter();
+    let target_iter = target_positions.clone().into_iter();
+
+    curr_iter
+        .zip(target_iter)
+        .map(|(curr, target)| curr * scalar + target * (1.0 - scalar))
+        .collect()
+}
+
+/// TODO: docs + implementation
+/// Retrieves the current position by using linear interpolation between the
+/// two nearest positions based on the starting time and current time.
+///
+/// # Arguments
+///
+/// * `motion` - Current `Motion`.
+fn get_positions(motion: &Motion, duration: &Duration) -> Option<JointArray<f32>> {
+    motion.get_frames(duration);
+    Some(JointArray::<f32>::default())
+}
+
+/// Executes the current motion.
+///
+/// # Arguments
+///
+/// * `nao_state` - State of the robot.
+/// * `motion_manager` - Keeps track of state needed for playing motions.
 #[system]
 fn motion_executer(nao_state: &mut NaoState, motion_manager: &mut MotionManager) -> Result<()> {
-    nao_state.position = JointArray::<f32>::default();
-    nao_state.stiffness = JointArray::<f32>::default();
+    if let Some(motion) = motion_manager.current_motion.clone() {
+        if !motion_manager.started_executing_motion {
+            if !reached_position(&nao_state.position, &motion.initial_positions, 0.05) {
+                // Starting position has not yet been reached, so lerp to start position, untill
+                // position has been reached.
+                // TODO: keep track of motion_starting_time and motion_execution_start_time.
+                return Ok(());
+            } else {
+                motion_manager.motion_starting_time = Some(SystemTime::now());
+            }
 
-    if let Some(motion_type) = &motion_manager.current_motion {
-        println!("test {:?}", motion_type);
-    }
+            match get_positions(
+                &motion,
+                &motion_manager
+                    .motion_starting_time
+                    .unwrap()
+                    .elapsed()
+                    .unwrap(),
+            ) {
+                Some(position) => {
+                    nao_state.position = JointArray::<f32>::default();
+                    // TODO: Add this to the motion files.
+                    nao_state.stiffness = JointArray::<f32>::default();
+                }
+                None => {
+                    //Current motion is finished.
+                    motion_manager.current_motion = None;
+                    motion_manager.motion_starting_time = None;
+                    motion_manager.started_executing_motion = false;
+                }
+            }
+        }
+    };
 
     Ok(())
 }
 
-pub struct MotionModule;
-impl Module for MotionModule {
-    fn initialize(self, app: App) -> Result<App> {
-        let motion_manager = MotionManager::new(HashMap::from([
-            (MotionType::SitDown, "./sit_down_motion.json"),
-            (MotionType::StandUp, "./stand_up_motion.json"),
-        ]));
+/// Initializes the `MotionManager`. Adds motions to the `MotionManger` by reading
+/// and deserializing the motions from motion files. Then adds the `MotionManager`
+/// as resource
+///
+/// # Arguments
+///
+/// * `storage` - System storage.
+fn motion_manager_initializer(storage: &mut Storage) -> Result<()> {
+    let mut motion_manager = MotionManager::new()
+        .add_motion(
+            MotionType::SitDownFromStand,
+            "./sit_down_from_stand_motion.json",
+        )?
+        .add_motion(
+            MotionType::StandUpFromSit,
+            "./stand_up_from_sit_motion.json",
+        )?;
 
-        app.add_system(motion_executer)
-            .add_resource(Resource::new(motion_manager))
+    // TODO: remove this, this is for testing
+    motion_manager.start_new_motion(MotionType::StandUpFromSit);
+    storage.add_resource(Resource::new(motion_manager))?;
+
+    Ok(())
+}
+
+/// Module used to add all necessary resource for playing motions to the system.
+pub struct MotionModule;
+
+impl Module for MotionModule {
+    /// Initializes the `MotionModule`. By adding the `motion_manager_initializer`
+    /// and the `motion_executor` to the system.
+    ///
+    /// # Arguments
+    ///
+    /// * `app` - App.
+    fn initialize(self, app: App) -> Result<App> {
+        Ok(app
+            .add_startup_system(motion_manager_initializer)?
+            .add_system(motion_executer))
     }
 }
