d726c57ca1d60fd0acb4bf210a1643f2fcccb4cd
jstwh <weerheimjoost2000@gmail.com>
2023-08-28 10:39:57 +0200

    Add Suggestions to WeeSound

--- yggdrasil/src/audio/wee_sound.rs
+++ yggdrasil/src/audio/wee_sound.rs
@@ -29,10 +29,11 @@ pub fn wee_sound_system(
     sounds: &mut SoundManager,
     has_ground_contact: &HasGroundContact,
 ) -> Result<()> {
-    if let Some(last_played) = play_sound.last_played {
-        if last_played.elapsed() < WEE_SOUND_TIMEOUT {
-            return Ok(());
-        }
+    if play_sound
+        .last_played
+        .is_some_and(|last_played| last_played.elapsed() < WEE_SOUND_TIMEOUT)
+    {
+        return Ok(());
     }
 
     if !**has_ground_contact && !play_sound.sound_played {
