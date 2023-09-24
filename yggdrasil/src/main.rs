23e8403fdd8781f94cec1ca6be7503126e5be99b
Aemulation <112875887+Aemulation@users.noreply.github.com>
2023-09-24 13:50:56 +0200

    Temporarily put a capture image test in main.rs

--- yggdrasil/src/main.rs
+++ yggdrasil/src/main.rs
@@ -2,6 +2,7 @@ use tyr::{prelude::*, tasks::TaskModule};
 
 use miette::Result;
 
+use heimdall::Camera;
 use yggdrasil::{
     audio::{sound_manager::SoundManagerModule, wee_sound::WeeSoundModule},
     filter::FilterModule,
@@ -12,6 +13,13 @@ fn main() -> Result<()> {
     tracing_subscriber::fmt::fmt().pretty().init();
 
     miette::set_panic_hook();
+
+    let mut camera = Camera::new("/dev/video0").unwrap();
+
+    for i in 0..10 {
+        let image = camera.get_image().unwrap();
+        image.store_jpeg(&format!("image-{}.jpeg", i)).unwrap();
+    }
 
     App::new()
         .add_module(TaskModule)?
