b051c8392a62bb65f0602760cfe5f6bca1582e71
Jakob Kaiser <jakob.j.jeep@gmail.com>
2023-10-08 17:45:27 +0200

    small changes

--- yggdrasil/src/behaviour/behaviour_engine/engine.rs
+++ yggdrasil/src/behaviour/behaviour_engine/engine.rs
@@ -26,6 +26,9 @@ pub enum BehaviourType {
 type Behaviour = Box<dyn ImplBehaviour + Sync + Send>;
 
 pub trait ImplBehaviour {
+    // Here we need to pass all information necessary for every possible behaviour.
+    // An alternative might be defining behaviours as systems so they can specify the data they
+    // need. However, in that case all those modules would be run every cycle which is a waist.
     fn execute(&self) -> NaoControlMessage;
 }
 
@@ -33,16 +36,6 @@ pub struct BehaviourEngine {
     current_behaviour: BehaviourType,
     behaviours: HashMap<BehaviourType, Behaviour>,
 }
-
-#[derive(Debug)]
-struct NoImplementationError;
-impl std::fmt::Display for NoImplementationError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Could not find implementation")
-    }
-}
-impl Error for NoImplementationError {}
-impl Diagnostic for NoImplementationError {}
 
 impl BehaviourEngine {
     pub fn new() -> Self {
@@ -52,14 +45,12 @@ impl BehaviourEngine {
         }
     }
 
-    pub fn execute_current_behaviour(&self) -> Result<()> {
+    pub fn execute_current_behaviour(&self) -> Result<NaoControlMessage> {
         if let Some(behaviour_implementation) = self.behaviours.get(&self.current_behaviour) {
-            behaviour_implementation.execute();
+            Ok(behaviour_implementation.execute())
         } else {
-            return Err(Report::new(NoImplementationError {}));
+            Err(Report::new(NoImplementationError {}))
         }
-
-        Ok(())
     }
 
     pub fn transition(&mut self, behaviour_type: BehaviourType) {
@@ -82,7 +73,45 @@ pub fn initializer(storage: &mut Storage) -> Result<()> {
 }
 
 #[system]
-pub fn executor(engine: &mut BehaviourEngine) -> Result<()> {
-    engine.execute_current_behaviour()?;
+pub fn executor(engine: &mut BehaviourEngine, ctrl_msg: &mut NaoControlMessage) -> Result<()> {
+    let NaoControlMessage {
+        position,
+        stiffness,
+        sonar,
+        left_ear,
+        right_ear,
+        chest,
+        left_eye,
+        right_eye,
+        left_foot,
+        right_foot,
+        skull,
+    } = engine.execute_current_behaviour()?;
+
+    // This can be overwritten over override field written by other modules,
+    // prob needs improvement
+    ctrl_msg.position = position;
+    ctrl_msg.stiffness = stiffness;
+    ctrl_msg.sonar = sonar;
+    ctrl_msg.left_ear = left_ear;
+    ctrl_msg.right_ear = right_ear;
+    ctrl_msg.chest = chest;
+    ctrl_msg.left_eye = left_eye;
+    ctrl_msg.right_eye = right_eye;
+    ctrl_msg.left_foot = left_foot;
+    ctrl_msg.right_foot = right_foot;
+    ctrl_msg.skull = skull;
+
     Ok(())
 }
+
+// Do not yet 100% understand how I should return errors
+#[derive(Debug)]
+struct NoImplementationError;
+impl std::fmt::Display for NoImplementationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Could not find implementation")
+    }
+}
+impl Error for NoImplementationError {}
+impl Diagnostic for NoImplementationError {}
