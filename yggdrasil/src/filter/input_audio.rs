a1201be0cdec37a4deb0a2f2a58f4b9df63d4768
ross <rmgeurtd@gmail.com>
2023-10-10 17:25:21 +0200

    added code to record audio, need to convert pcm to vec to save in InputAudio

--- yggdrasil/src/filter/input_audio.rs
+++ yggdrasil/src/filter/input_audio.rs
@@ -1,16 +1,16 @@
 use miette::Result;
 use tyr::prelude::*;
 use alsa::pcm::*;
-use alsa::Direction;
+use alsa::{Direction, ValueOr};
 
 pub struct InputAudioFilter;
 
 // constants
 // sample rate , frames, format, access
-const SAMPLE_RATE: i32 = 44100;
-const FRAMES: i32 = 2048;
-const FORMAT: &str = "FloatLE";
-const ACCESS: &str = "RWInterleaved";
+const SAMPLE_RATE: u32 = 44100;
+const FRAMES: i64 = 2048;
+const FORMAT: Format = Format::s16();
+const ACCESS: Access = Access::RWInterleaved;
 
 impl Module for InputAudioFilter {
     fn initialize(self, app: App) -> Result<App> {
@@ -29,5 +29,16 @@ pub struct InputAudio{
 fn input_audio_filter(
     input_audio: &mut InputAudio) -> Result<()> {
     // record audio, store new audio in struct
+    let pcm = PCM::new("default", Direction::Capture, false).unwrap();
+    {
+        let hwp = HwParams::any(&pcm).unwrap();
+        hwp.set_rate(SAMPLE_RATE, ValueOr::Nearest).unwrap();
+        hwp.set_format(FORMAT).unwrap();
+        hwp.set_access(ACCESS).unwrap();
+        hwp.set_period_size(FRAMES, ValueOr::Nearest).unwrap();
+        pcm.hw_params(&hwp).unwrap();
+    }
+    pcm.start().unwrap();
+    // input_audio.audio = pcm;
     Ok(())
 }
