15dc12b3b9d21908dd1506b4170f57ff56372654
Aemulation <112875887+Aemulation@users.noreply.github.com>
2023-10-16 15:00:34 +0200

    Cleanup benchmark

--- crates/heimdall/benches/rgb_conversion_bench.rs
+++ crates/heimdall/benches/rgb_conversion_bench.rs
@@ -1,15 +1,15 @@
 use std::io::Write;
 
-use heimdall::{Camera, RgbImage};
+use heimdall::{Camera, RgbImage, CAMERA_TOP};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut camera = Camera::new("/dev/video0").unwrap();
+    let mut camera = Camera::new(CAMERA_TOP).unwrap();
     let image = camera.get_yuyv_image().unwrap();
 
     let mut file = std::fs::File::create("image.raw").unwrap();
-    file.write_all(&image[..]).unwrap();
+    file.write_all(&image).unwrap();
 
     let mut rgb_image = RgbImage::new();
 
