d384654fc50a67042caf5c512b61d99d9dad8a7d
Aemulation <112875887+Aemulation@users.noreply.github.com>
2023-10-16 14:59:40 +0200

    Improve example

--- crates/heimdall/examples/capture_image.rs
+++ crates/heimdall/examples/capture_image.rs
@@ -1,18 +1,16 @@
 use std::io::Write;
 
-use heimdall::{Camera, Result, RgbImage};
+use heimdall::{Camera, Result, RgbImage, CAMERA_TOP};
 
 fn main() -> Result<()> {
-    let mut camera = Camera::new("/dev/video0")?;
+    let mut camera = Camera::new(CAMERA_TOP)?;
     let image = camera.get_yuyv_image()?;
 
-    let mut file = std::fs::File::create("image.raw")?;
-    file.write_all(&image[..])?;
+    let mut file = std::fs::File::create("yuyv_image.raw")?;
+    file.write_all(&image)?;
 
     let mut rgb_image = RgbImage::new();
-    for _ in 0..100 {
-        image.to_rgb(&mut rgb_image)?;
-    }
+    image.to_rgb(&mut rgb_image)?;
 
     image.store_jpeg("image.jpeg")?;
 
