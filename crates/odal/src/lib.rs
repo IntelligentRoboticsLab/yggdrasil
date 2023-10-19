a339ab5ec6ba9829545dccb4574d738770a20fe7
Joost Weerheim <weerheimjoost2000@gmail.com>
2023-10-20 00:19:41 +0200

    Add suggestions & cleanup

--- crates/odal/src/lib.rs
+++ crates/odal/src/lib.rs
@@ -1,39 +1,34 @@
 use miette::Result;
 use std::fs;
-use toml::macros::Deserialize;
 use toml::Table;
+use toml::{from_str, macros::Deserialize, Value};
 use tyr::prelude::*;
 
-
+/// Configuration provides functionality for saving and loading configurations.
 pub trait Configuration: Sized + for<'de> Deserialize<'de> {
-    // Path to a root config
+    /// Path to a root config
     const PATH: &'static str;
 
-    fn load(overlay_path: &str) -> Result<Self> {
-        let main_cfg: Table = toml::from_str(&fs::read_to_string(Self::PATH).unwrap()).unwrap();
-
-        let overlay_cfg: Table = toml::from_str(&fs::read_to_string(overlay_path).unwrap()).unwrap();
-
-        let add_keys: bool = false;
-
-        let generated_cfg: Self = toml::from_str(&generate_config(main_cfg, overlay_cfg, add_keys).to_string()).unwrap();
-
-        Ok(generated_cfg)
+    /// Generates a config from an overlay and a root config.
+    fn load(overlay_path: &str) -> Self {
+        let main_cfg: Table = from_str(&fs::read_to_string(Self::PATH).unwrap()).unwrap();
+        let overlay_cfg: Table = from_str(&fs::read_to_string(overlay_path).unwrap()).unwrap();
+        let generated_cfg: Self =
+            from_str(&generate_config(main_cfg, overlay_cfg, false).to_string()).unwrap();
+        generated_cfg
     }
 
+    /// Adds new K/V pairs to an overlay or overwrites existing pairs with the requested updates.
     fn save(overlay_path: &str, updates: Table) {
-        let overlay_cfg: Table = toml::from_str(&fs::read_to_string(overlay_path).unwrap()).unwrap();
-
-        let add_keys: bool = true;
-
-        let result = generate_config(overlay_cfg, updates, add_keys);
-
+        let overlay_cfg: Table = from_str(&fs::read_to_string(overlay_path).unwrap()).unwrap();
+        let result = generate_config(overlay_cfg, updates, true);
         fs::write(overlay_path, result.to_string()).unwrap();
     }
 }
 
+/// ConfigResource provides functionality for adding configurations as resources to App.
 pub trait ConfigResource {
-    /// Wrapper around Resource in order to add threadsafe configurations to the app.
+    /// Wrapper around Resource in order to add configurations to the app.
     fn add_config<T: Configuration + Send + Sync + 'static + for<'de> Deserialize<'de>>(
         self,
         path: &str,
@@ -62,12 +57,12 @@ pub fn generate_config(main: Table, overlay: Table, add_keys: bool) -> Table {
         match overlay.get(&k) {
             Some(overlay_value) => {
                 // If the key exists in overlay, check if it's also a table
-                if let toml::Value::Table(main_table) = v {
-                    if let toml::Value::Table(overlay_table) = overlay_value {
+                if let Value::Table(main_table) = v {
+                    if let Value::Table(overlay_table) = overlay_value {
                         // Recursively merge the subtables
                         let merged_table =
                             generate_config(main_table, overlay_table.clone(), add_keys);
-                        generated_toml.insert(k, toml::Value::Table(merged_table));
+                        generated_toml.insert(k, Value::Table(merged_table));
                     }
                 } else {
                     // If it's not a table, use the overlay value
