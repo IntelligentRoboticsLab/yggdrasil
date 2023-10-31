2653def185d1afdb59d656640033087e1e00c583
Jakob Kaiser <jakob.j.jeep@gmail.com>
2023-10-31 13:41:41 +0100

    refactoring

--- yggdrasil/src/behaviour/behaviour_engine/engine.rs
+++ yggdrasil/src/behaviour/behaviour_engine/engine.rs
@@ -1,148 +1,109 @@
-// TODO: put in RFC
-//
-// Thoughts:
-//
-// Right now we would need to pass all information necessary for every possible behaviour into the
-// execute function of each behaviour implemenation, which is not ideal. Especially when behaviour
-// modules are statefull and need to keep track
-// of their own state this might become a little chaotic and makes it unpleasant to add new
-// behaviours that would depend on new states. We could probably wrap all the data in a single
-// struct to make this less cumbersome.
-//
-// An alternative might be defining behaviours as systems so they can specify the data they
-// need. This means every behaviour would run every cycle. We would then only use the results
-// we actually need. Will make it a lot easier to add new behaviours with their own states.
-// However, everything is then always run which is a bit of a waist of computation. If we
-// decide that this computation is not super expensive then this would probably be the best option.
-//
-//
-// Something that is not entirely clear for me yet is how we would incorporate for example the
-// walking engine. Currently the walking engine directly writes to the nao control message. Maybe
-// the walking engine could be dependent on the Behaviour state for computing the correct joint
-// values then store the resulting joint values in some intermediate representation which is then
-// used by the behaviour modules. Lets say that in a behaviour we want to walk to a specific position, how would we pass that
-// information to the walking engine in this case?
-//
-// A solution might be one similar to what hulks has, the result of a behaviour is a motion
-// command. Which is then used by other modules for computing the correct joint angles.
-
-// Instead of hashmap, use a match statement without dymanic dispatch
-// Add context
-// Add Behaviour states
-
-use std::error::Error;
-
 use crate::{
-    behaviour::Role,
-    behaviour::primary_state::PrimaryState,
+    behaviour::{PrimaryState, Role},
     game_phase::GamePhase,
 };
 
-use miette::{Diagnostic, Result};
+use miette::Result;
 use nidhogg::NaoControlMessage;
 use tyr::prelude::*;
 
 use crate::behaviour::behaviour_engine::behaviours::*;
+use crate::behaviour::behaviour_engine::transitions::*;
 
-#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
-pub enum BehaviourType {
-    Defend,
-    Dribble,
-    Falling,
-    GetReadyForKickOff,
-    GetReadyForPenalty,
-    LookForBall,
-    LookAround(LookAroundState),
+#[derive(Debug)]
+pub enum Behaviour {
+    Example(ExampleBehaviour),
     None,
-    Penalized,
-    Stand,
-    Sit,
-    Unstiff,
-    WalkToBall,
 }
 
-// type Behaviour = Box<dyn ImplBehaviour + Sync + Send>;
-
-pub trait ImplBehaviour {
-    fn execute(&self, ctx: &mut BehaviourContext) -> NaoControlMessage;
-}
-
+#[derive(Debug)]
 pub struct BehaviourEngine {
-    current_behaviour: BehaviourType,
+    current_behaviour: Behaviour,
 }
 
-// struct BehaviourContext;
-#[derive(Debug, Clone, Copy)]
-pub struct BehaviourContext {
-    pub ball: [f64; 3],
-    pub game_phase: GamePhase,
-    pub current_behaviour: BehaviourType,
-    pub robot_pos: [f64; 3],
-    pub player_number: i32,
-    pub primary_state: PrimaryState,
-    pub role: Role,
+impl Default for BehaviourEngine {
+    fn default() -> Self {
+        BehaviourEngine {
+            current_behaviour: Behaviour::None,
+        }
+    }
 }
 
 impl BehaviourEngine {
-    pub fn new() -> Self {
-        BehaviourEngine {
-            current_behaviour: BehaviourType::None
-        }
-    }
-
-    pub fn execute_current_behaviour(&self, ctx: &mut BehaviourContext) -> Result<NaoControlMessage, i32> {
+    pub fn execute_current_behaviour(
+        &mut self,
+        ctx: &mut BehaviourContext,
+        ctrl_msg: &mut NaoControlMessage,
+    ) {
+        //TODO: just use dynamic dispatch instead? Seems cleaner, then the entire match statement
+        //is not necessary and we can just directly call behaviour.execute().
+        use Behaviour::*;
         match self.current_behaviour {
-            BehaviourType::LookAround(_) => Ok(LookAround.execute(ctx)),
-            _ => Err(0),
+            Example(ref mut behaviour) => behaviour.execute(ctx, ctrl_msg),
+            None => (),
         }
     }
 
-    pub fn transition(&mut self, behaviour_type: BehaviourType) {
+    pub fn transition(&mut self, behaviour_type: Behaviour) {
         self.current_behaviour = behaviour_type;
     }
+}
+
+#[derive(Debug)]
+pub struct BehaviourContext<'a> {
+    pub game_phase: &'a GamePhase,
+    pub primary_state: &'a PrimaryState,
+    pub role: &'a Role,
+}
+
+pub trait ImplBehaviour {
+    fn execute(&mut self, ctx: &mut BehaviourContext, ctrl_msg: &mut NaoControlMessage);
 }
 
 #[system]
 pub fn executor(
     engine: &mut BehaviourEngine,
     ctrl_msg: &mut NaoControlMessage,
-    //ball_positio
-    // ball_position: Vec<f32>,
-    // robot_position: Vec<32>,
-    behaviour_context: &mut BehaviourContext, // Pass all information needed to the engine
+    role: &Role,
+    game_phase: &GamePhase,
+    primary_state: &PrimaryState,
 ) -> Result<()> {
-    let mut ctx = *behaviour_context;
-
-    // pass to this instad of
-    let message = match engine.execute_current_behaviour(&mut ctx) {
-        Err(_e) => NaoControlMessage::default(),
-        Ok(res) => res,
+    let mut ctx = BehaviourContext {
+        primary_state: &primary_state,
+        game_phase: &game_phase,
+        role: &role,
     };
 
-    *ctrl_msg = message;
-
-    // ctrl_msg.position = position;
-        // ctrl_msg.stiffness = stiffness;
-        // ctrl_msg.sonar = sonar;
-        // ctrl_msg.left_ear = left_ear;
-        // ctrl_msg.right_ear = right_ear;
-    // ctrl_msg.chest = chest;
-        // ctrl_msg.left_eye = left_eye;
-        // ctrl_msg.right_eye = right_eye;
-        // ctrl_msg.left_foot = left_foot;
-        // ctrl_msg.right_foot = right_foot;
-        // ctrl_msg.skull = skull;
+    engine.execute_current_behaviour(&mut ctx, &mut ctrl_msg);
 
     Ok(())
 }
 
-// Do not yet 100% understand how I should return errors
 #[derive(Debug)]
-struct NoImplementationError;
-impl std::fmt::Display for NoImplementationError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Could not find implementation")
-    }
+pub struct TransitionContext<'a> {
+    pub game_phase: &'a GamePhase,
+    pub primary_state: &'a PrimaryState,
+    pub role: &'a Role,
 }
-impl Error for NoImplementationError {}
-impl Diagnostic for NoImplementationError {}
+
+#[system]
+pub fn transition_behaviour(
+    role: &Role,
+    engine: &mut BehaviourEngine,
+    game_phase: &GamePhase,
+    primary_state: &PrimaryState,
+) -> Result<()> {
+    //TODO is there a better way to do this?
+    let ctx = TransitionContext {
+        role: &role,
+        primary_state: &primary_state,
+        game_phase: &game_phase,
+    };
+
+    use Role::*;
+    match *role {
+        ExampleRole => update_example_role_behaviour(&mut engine, &ctx),
+    }
+
+    Ok(())
+}
