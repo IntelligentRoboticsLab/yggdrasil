9bd360f59bc72e983a40f9451d26a3884d7368fa
Jakob Kaiser <jakob.j.jeep@gmail.com>
2023-11-23 18:15:24 +0100

    Renamed transition parameter

--- yggdrasil/src/behaviour/behaviour_engine/transitions/keeper.rs
+++ yggdrasil/src/behaviour/behaviour_engine/transitions/keeper.rs
@@ -1,9 +1,12 @@
 use crate::behaviour::behaviour_engine::engine::{Behaviour, BehaviourContext, BehaviourState};
 use crate::game_phase::GamePhase;
 
-pub fn transition_keeper_role_behaviour(state: Behaviour, context: &BehaviourContext) -> Behaviour {
+pub fn transition_keeper_role_behaviour(
+    behaviour: Behaviour,
+    context: &BehaviourContext,
+) -> Behaviour {
     use Behaviour as B;
-    match (state, context.game_phase) {
+    match (behaviour, context.game_phase) {
         (B::InitialBehaviour(state), GamePhase::Normal) => B::ExampleBehaviour(state.into()),
         (B::InitialBehaviour(state), GamePhase::Timeout) => B::ExampleBehaviour(state.into()),
         _ => B::InitialBehaviour(BehaviourState::default()),
