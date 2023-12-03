a6fb725c51dfc990cebad889549ad8a6869850f6
Julian Blaauboer <44556294+julian-blaauboer@users.noreply.github.com>
2023-12-03 13:00:21 +0100

    `tyr-internal`: Add `DebugView` for debuggable resources (#67)
    Co-authored-by: Gijs de Jong <berichtaangijs@gmail.com>
    Co-authored-by: Harold Ruiter <haroldruiter28@gmail.com>

--- crates/tyr/tyr_internal/src/app.rs
+++ crates/tyr/tyr_internal/src/app.rs
@@ -123,6 +123,36 @@ impl App {
         Ok(self)
     }
 
+    /// Creates a [`Resource<T>`] on `T`s that implement [`Default`] and adds it to the app storage.
+    ///
+    /// This method is used to mark [`Resource<T>`] as a debuggable resource, making it
+    /// show up in the debug panel.
+    ///
+    /// # Errors
+    /// This function fails if there is already a resource of type `T` in the storage.
+    pub fn init_debuggable_resource<T: std::fmt::Debug + Default + Send + Sync + 'static>(
+        mut self,
+    ) -> Result<Self> {
+        self.storage
+            .add_debuggable_resource(Resource::<T>::default())?;
+        Ok(self)
+    }
+
+    /// Consumes the [`Resource<T>`] and adds it to the app storage.
+    ///
+    /// This method is used to mark [`Resource<T>`] as a debuggable resource, making it
+    /// show up in the debug panel.
+    ///
+    /// # Errors
+    /// This functions fails if there is already a resource of type `T` in the storage.
+    pub fn add_debuggable_resource<T: std::fmt::Debug + Send + Sync + 'static>(
+        mut self,
+        res: Resource<T>,
+    ) -> Result<Self> {
+        self.storage.add_debuggable_resource(res)?;
+        Ok(self)
+    }
+
     /// Consumes the [`Module`] and incorporates it into the app.
     /// The module must implement the [`Module`] trait, which defines the [`.initialize()`](`Module::initialize`) method.
     /// The [`.initialize()`](`Module::initialize`) method allows the [`Module`] to add resource and systems to the app.
