5e47b7d3ff8d88ba6e6a3e1438b56fbb5e678d91
Aemulation <112875887+Aemulation@users.noreply.github.com>
2023-12-08 00:23:35 +0100

    Simplify `camera_system` function

--- yggdrasil/src/camera/mod.rs
+++ yggdrasil/src/camera/mod.rs
@@ -89,17 +89,13 @@ fn camera_system(
 ) -> Result<()> {
     if let Some(new_top_image) = top_image_task.poll() {
         *top_image = new_top_image?;
-        top_image_task.try_spawn(receive_top_image(top_camera.0.clone()))?;
-    } else {
-        top_image_task.try_spawn(receive_top_image(top_camera.0.clone()))?;
     }
+    top_image_task.try_spawn(receive_top_image(top_camera.0.clone()))?;
 
     if let Some(new_bottom_image) = bottom_image_task.poll() {
         *bottom_image = new_bottom_image?;
-        bottom_image_task.try_spawn(receive_bottom_image(bottom_camera.0.clone()))?;
-    } else {
-        bottom_image_task.try_spawn(receive_bottom_image(bottom_camera.0.clone()))?;
     }
+    bottom_image_task.try_spawn(receive_bottom_image(bottom_camera.0.clone()))?;
 
     Ok(())
 }
