1a93f55afac655b6330b56a22a34f85786edc037
Aemulation <112875887+Aemulation@users.noreply.github.com>
2023-12-09 20:22:04 +0100

    Improve comments of some constants

--- yggdrasil/src/camera/mod.rs
+++ yggdrasil/src/camera/mod.rs
@@ -8,9 +8,17 @@ use tyr::prelude::*;
 use heimdall::{Camera, YuyvImage};
 
 /// This variable specifies how many `TopImage`'s' can be alive at the same time.
+///
+/// It is recommended to have at least one more buffer than is required. This way, the next frame
+/// from the camera can already be stored in a buffer, reducing the latency between destructing a
+/// `TopImage` and being able to fetch the newest `TopImage`.
 const NUMBER_OF_TOP_CAMERA_BUFFERS: u32 = 2;
 
 /// This variable specifies how many `BottomImage`'s' can be alive at the same time.
+///
+/// It is recommended to have at least one more buffer than is required. This way, the next frame
+/// from the camera can already be stored in a buffer, reducing the latency between destructing a
+/// `BottomImage` and being able to fetch the newest `BottomImage`.
 const NUMBER_OF_BOTTOM_CAMERA_BUFFERS: u32 = 2;
 
 /// This module captures images using the top- and bottom camera of the NAO.
