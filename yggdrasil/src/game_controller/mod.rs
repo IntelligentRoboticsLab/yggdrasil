34248b9a4e81db30311cb8af13550f9da755e34d
Aemulation <112875887+Aemulation@users.noreply.github.com>
2023-12-11 23:16:33 +0100

    Small refactor of `mod.rs`

--- yggdrasil/src/game_controller/mod.rs
+++ yggdrasil/src/game_controller/mod.rs
@@ -13,16 +13,21 @@ pub struct GameControllerModule;
 
 impl Module for GameControllerModule {
     fn initialize(self, app: App) -> Result<App> {
-        let game_controller_socket = Resource::new(Arc::new(Mutex::new(
-            UdpSocket::bind(SocketAddrV4::new(
-                Ipv4Addr::UNSPECIFIED,
-                GAMECONTROLLER_DATA_PORT,
-            ))
-            .into_diagnostic()?,
-        )));
+        let game_controller_socket = UdpSocket::bind(SocketAddrV4::new(
+            Ipv4Addr::UNSPECIFIED,
+            GAMECONTROLLER_DATA_PORT,
+        ))
+        .into_diagnostic()?;
+
+        // game_controller_socket
+        //     .set_nonblocking(true)
+        //     .into_diagnostic()?;
+
+        let game_controller_socket_resource =
+            Resource::new(Arc::new(Mutex::new(game_controller_socket)));
 
         Ok(app
-            .add_resource(game_controller_socket)?
+            .add_resource(game_controller_socket_resource)?
             .add_module(receive::GameControllerReceiveModule)?
             .add_module(transmit::GameControllerSendModule)?)
     }
