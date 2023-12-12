425d3e83dae0c8263cc1fab86fe0fb1c066b9da5
Aemulation <112875887+Aemulation@users.noreply.github.com>
2023-12-12 19:11:31 +0100

    Don't throw "waiting" error in game controller socket

--- yggdrasil/src/game_controller/receive.rs
+++ yggdrasil/src/game_controller/receive.rs
@@ -3,12 +3,15 @@ use super::GameControllerSocket;
 use bifrost::communication::RoboCupGameControlData;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
 use bifrost::serialization::Decode;
 
 use miette::{IntoDiagnostic, Result};
 
 use tyr::prelude::*;
+
+const GAMECONTROLLER_RECEIVE_DELAY_MS: u64 = 250;
 
 pub struct GameControllerReceiveModule;
 
@@ -30,15 +33,24 @@ async fn receive_message(
 ) -> Result<(RoboCupGameControlData, SocketAddr)> {
     let mut buffer = [0u8; 1024];
 
-    let (_bytes_received, game_controller_address) = game_controller_socket
-        .lock()
-        .unwrap()
-        .recv_from(&mut buffer)
-        .into_diagnostic()?;
+    loop {
+        match game_controller_socket
+            .lock()
+            .unwrap()
+            .recv_from(&mut buffer)
+        {
+            Ok((_bytes_received, game_controller_address)) => {
+                let message =
+                    RoboCupGameControlData::decode(&mut buffer.as_slice()).into_diagnostic()?;
 
-    let message = RoboCupGameControlData::decode(&mut buffer.as_slice()).into_diagnostic()?;
+                return Ok((message, game_controller_address));
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {}
+            Err(err) => Err(err).into_diagnostic()?,
+        };
 
-    Ok((message, game_controller_address))
+        std::thread::sleep(Duration::from_millis(GAMECONTROLLER_RECEIVE_DELAY_MS));
+    }
 }
 
 #[system]
@@ -52,11 +64,15 @@ pub(crate) fn receive_system(
         receive_message_task
             .try_spawn(receive_message(game_controller_socket.clone()))
             .into_diagnostic()?;
-    } else if let Some(Ok((new_game_controller_message, new_game_controller_address))) =
-        receive_message_task.poll()
-    {
-        *game_controller_message = Some(new_game_controller_message);
-        *game_controller_address = Some(new_game_controller_address);
+    } else {
+        match receive_message_task.poll() {
+            Some(Ok((new_game_controller_message, new_game_controller_address))) => {
+                *game_controller_message = Some(new_game_controller_message);
+                *game_controller_address = Some(new_game_controller_address);
+            }
+            Some(Err(err)) => eprintln!("Failed to decode game controller message: {err}"),
+            None => {}
+        }
     }
 
     Ok(())
