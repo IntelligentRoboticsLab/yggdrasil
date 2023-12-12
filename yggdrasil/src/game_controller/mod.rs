6a249f0e201f9688c570e589c8d241c27d981687
Aemulation <112875887+Aemulation@users.noreply.github.com>
2023-12-12 21:44:03 +0100

    Refactor `mod.rs`

--- yggdrasil/src/game_controller/mod.rs
+++ yggdrasil/src/game_controller/mod.rs
@@ -18,8 +18,8 @@ pub(crate) struct GameControllerData {
 
 pub struct GameControllerModule;
 
-impl Module for GameControllerModule {
-    fn initialize(self, app: App) -> Result<App> {
+impl GameControllerModule {
+    fn new_game_controller_socket() -> Result<UdpSocket> {
         let game_controller_socket = UdpSocket::bind(SocketAddrV4::new(
             Ipv4Addr::UNSPECIFIED,
             GAMECONTROLLER_DATA_PORT,
@@ -30,18 +30,30 @@ impl Module for GameControllerModule {
             .set_nonblocking(true)
             .into_diagnostic()?;
 
-        let game_controller_data = GameControllerData {
+        Ok(game_controller_socket)
+    }
+
+    fn new_game_controller_data() -> Result<GameControllerData> {
+        let game_controller_socket = Self::new_game_controller_socket()?;
+
+        Ok(GameControllerData {
             last_send_message_instant: Instant::now(),
             socket: game_controller_socket,
             game_controller_address: None,
-        };
+        })
+    }
+}
+
+impl Module for GameControllerModule {
+    fn initialize(self, app: App) -> Result<App> {
+        let game_controller_data = Self::new_game_controller_data()?;
+
         let game_controller_data_resource = Resource::<Arc<Mutex<GameControllerData>>>::new(
             Arc::new(Mutex::new(game_controller_data)),
         );
 
-        Ok(app
-            .add_resource(game_controller_data_resource)?
+        app.add_resource(game_controller_data_resource)?
             .add_module(receive::GameControllerReceiveModule)?
-            .add_module(transmit::GameControllerSendModule)?)
+            .add_module(transmit::GameControllerSendModule)
     }
 }
