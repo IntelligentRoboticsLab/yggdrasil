d799192ec259d5f280afc90a30cc67e746f9202b
Aemulation <112875887+Aemulation@users.noreply.github.com>
2023-12-12 20:31:21 +0100

    Get rid of the game controller tasks

--- yggdrasil/src/game_controller/receive.rs
+++ yggdrasil/src/game_controller/receive.rs
@@ -3,15 +3,12 @@ use super::GameControllerSocket;
 use bifrost::communication::RoboCupGameControlData;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
 
 use bifrost::serialization::Decode;
 
 use miette::{IntoDiagnostic, Result};
 
 use tyr::prelude::*;
-
-const GAMECONTROLLER_RECEIVE_DELAY_MS: u64 = 250;
 
 pub struct GameControllerReceiveModule;
 
@@ -23,33 +20,7 @@ impl Module for GameControllerReceiveModule {
         Ok(app
             .add_resource(game_controller_receive_message)?
             .add_resource(game_controller_address)?
-            .add_task::<AsyncTask<Result<(RoboCupGameControlData, SocketAddr)>>>()?
             .add_system(receive_system))
-    }
-}
-
-async fn receive_message(
-    game_controller_socket: Arc<Mutex<GameControllerSocket>>,
-) -> Result<(RoboCupGameControlData, SocketAddr)> {
-    let mut buffer = [0u8; 1024];
-
-    loop {
-        match game_controller_socket
-            .lock()
-            .unwrap()
-            .recv_from(&mut buffer)
-        {
-            Ok((_bytes_received, game_controller_address)) => {
-                let message =
-                    RoboCupGameControlData::decode(&mut buffer.as_slice()).into_diagnostic()?;
-
-                return Ok((message, game_controller_address));
-            }
-            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {}
-            Err(err) => Err(err).into_diagnostic()?,
-        };
-
-        std::thread::sleep(Duration::from_millis(GAMECONTROLLER_RECEIVE_DELAY_MS));
     }
 }
 
@@ -58,22 +29,24 @@ pub(crate) fn receive_system(
     game_controller_message: &mut Option<RoboCupGameControlData>,
     game_controller_socket: &mut Arc<Mutex<GameControllerSocket>>,
     game_controller_address: &mut Option<SocketAddr>,
-    receive_message_task: &mut AsyncTask<Result<(RoboCupGameControlData, SocketAddr)>>,
 ) -> Result<()> {
-    if !receive_message_task.active() {
-        receive_message_task
-            .try_spawn(receive_message(game_controller_socket.clone()))
-            .into_diagnostic()?;
-    } else {
-        match receive_message_task.poll() {
-            Some(Ok((new_game_controller_message, new_game_controller_address))) => {
-                *game_controller_message = Some(new_game_controller_message);
-                *game_controller_address = Some(new_game_controller_address);
-            }
-            Some(Err(err)) => eprintln!("Failed to decode game controller message: {err}"),
-            None => {}
-        }
-    }
+    let mut buffer = [0u8; 1024];
 
-    Ok(())
+    match game_controller_socket
+        .lock()
+        .unwrap()
+        .recv_from(&mut buffer)
+    {
+        Ok((_bytes_received, new_game_controller_address)) => {
+            let new_game_controller_message =
+                RoboCupGameControlData::decode(&mut buffer.as_slice()).into_diagnostic()?;
+
+            *game_controller_message = Some(new_game_controller_message);
+            *game_controller_address = Some(new_game_controller_address);
+
+            Ok(())
+        }
+        Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => Ok(()),
+        Err(err) => Err(err).into_diagnostic()?,
+    }
 }
--- yggdrasil/src/game_controller/transmit.rs
+++ yggdrasil/src/game_controller/transmit.rs
@@ -17,31 +17,38 @@ pub struct GameControllerSendModule;
 
 impl Module for GameControllerSendModule {
     fn initialize(self, app: App) -> Result<App> {
-        Ok(app
-            .add_task::<AsyncTask<Result<RoboCupGameControlReturnData>>>()?
-            .add_system(send_system))
+        Ok(app.add_system(send_system))
     }
 }
 
-async fn send_message(
-    game_controller_socket: Arc<Mutex<GameControllerSocket>>,
-    mut game_controller_address: SocketAddr,
-    game_controller_transmit_data: Arc<Mutex<GameControllerData>>,
-) -> Result<RoboCupGameControlReturnData> {
+#[system]
+pub(crate) fn send_system(
+    game_controller_socket: &mut Arc<Mutex<GameControllerSocket>>,
+    game_controller_return_address: &Option<SocketAddr>,
+    game_controller_transmit_data: &Arc<Mutex<GameControllerData>>,
+) -> Result<()> {
+    let Some(mut game_controller_address) = game_controller_return_address else {
+        return Ok(());
+    };
+
     let mut buffer = [0u8; 1024];
 
     game_controller_address.set_port(GAMECONTROLLER_RETURN_PORT);
     let game_controller_message =
         RoboCupGameControlReturnData::new(2, 8, 0, [0f32; 3], -1f32, [0f32; 2]);
 
-    let duration_to_sleep = game_controller_transmit_data
+    let duration_to_wait = game_controller_transmit_data
         .lock()
         .unwrap()
         .last_send_message_instant
         .add(Duration::from_millis(GAMECONTROLLER_RETURN_DELAY_MS))
         .duration_since(Instant::now());
 
-    std::thread::sleep(duration_to_sleep);
+    std::thread::sleep(duration_to_wait);
+
+    if !duration_to_wait.is_zero() {
+        return Ok(());
+    }
 
     game_controller_message
         .encode(buffer.as_mut_slice())
@@ -57,29 +64,6 @@ async fn send_message(
         .lock()
         .unwrap()
         .last_send_message_instant = Instant::now();
-
-    Ok(game_controller_message)
-}
-
-#[system]
-pub(crate) fn send_system(
-    game_controller_socket: &mut Arc<Mutex<GameControllerSocket>>,
-    game_controller_return_address: &Option<SocketAddr>,
-    send_message_task: &mut AsyncTask<Result<RoboCupGameControlReturnData>>,
-    game_controller_transmit_data: &Arc<Mutex<GameControllerData>>,
-) -> Result<()> {
-    if let Some(game_controller_return_address) = game_controller_return_address {
-        if !send_message_task.active() {
-            send_message_task
-                .try_spawn(send_message(
-                    game_controller_socket.clone(),
-                    *game_controller_return_address,
-                    game_controller_transmit_data.clone(),
-                ))
-                .into_diagnostic()?;
-        } else if let Some(_new_game_controller_message) = send_message_task.poll() {
-        }
-    }
 
     Ok(())
 }
