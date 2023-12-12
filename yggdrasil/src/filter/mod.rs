afe4d60e58ef7eba2a09815f00ae5b79d019603c
JinseiSteven <stephanfknvisser@gmail.com>
2023-12-12 15:24:33 +0100

    added imu measurements

--- yggdrasil/src/filter/mod.rs
+++ yggdrasil/src/filter/mod.rs
@@ -1,10 +1,14 @@
 use crate::prelude::*;
 
-use self::{button::ButtonFilter, fsr::FSRFilter, imu::IMUFilter, sonar::SonarFilter};
+use self::{
+    button::ButtonFilter, falling::FallingFilter, fsr::FSRFilter, imu::IMUFilter,
+    sonar::SonarFilter,
+};
 
 #[cfg(feature = "alsa")]
 pub mod audio_input;
 pub mod button;
+pub mod falling;
 pub mod fsr;
 pub mod imu;
 pub mod sonar;
@@ -16,6 +20,7 @@ impl Module for FilterModule {
         let app = app
             .add_module(ButtonFilter)?
             .add_module(FSRFilter)?
+            .add_module(FallingFilter)?
             .add_module(IMUFilter)?
             .add_module(SonarFilter)?;
 
