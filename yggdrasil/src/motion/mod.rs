926a1b65f1bf764e5b0861dcfa39548defba4f59
JinseiSteven <stephanfknvisser@gmail.com>
2023-12-12 13:44:25 +0100

    Motion module imported

---
+++ yggdrasil/src/motion/mod.rs
@@ -0,0 +1,28 @@
+use crate::nao::write_hardware_info;
+use miette::Result;
+use tyr::prelude::*;
+
+pub mod motion_executer;
+pub mod motion_manager;
+pub mod motion_types;
+pub mod motion_util;
+
+use motion_executer::motion_executer;
+use motion_manager::motion_manager_initializer;
+
+/// Module used to add all necessary resource for playing motions to the system.
+pub struct MotionModule;
+
+impl Module for MotionModule {
+    /// Initializes the `MotionModule`. By adding the `motion_manager_initializer`
+    /// and the `motion_executor` to the system.
+    ///
+    /// # Arguments
+    ///
+    /// * `app` - App.
+    fn initialize(self, app: App) -> Result<App> {
+        Ok(app
+            .add_startup_system(motion_manager_initializer)?
+            .add_system(motion_executer.after(write_hardware_info)))
+    }
+}
