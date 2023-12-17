77594220b96e070046d6ec8be068c39d4591375c
Harold <37922636+YukumoHunter@users.noreply.github.com>
2023-12-17 12:57:38 +0100

    Derive `miette::Diagnostic` for `heimdall::Error` (#83)

--- crates/heimdall/src/error.rs
+++ crates/heimdall/src/error.rs
@@ -1,11 +1,12 @@
 //! Result and Error types for the crate.
+use miette::Diagnostic;
 use thiserror::Error;
 
 /// Result containing an error variant from this module.
 pub type Result<T> = std::result::Result<T, Error>;
 
 /// Camera error variants.
-#[derive(Error, Debug)]
+#[derive(Error, Debug, Diagnostic)]
 pub enum Error {
     /// IO error, this wraps a [std::io::Error]
     #[error(transparent)]
