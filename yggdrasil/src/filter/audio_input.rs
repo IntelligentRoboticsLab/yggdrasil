fe7d319728f990ce6ac3ef072e4db040a4ad71a7
Aemulation <112875887+Aemulation@users.noreply.github.com>
2024-02-01 12:47:20 +0100

    Remove `expect()`

--- yggdrasil/src/filter/audio_input.rs
+++ yggdrasil/src/filter/audio_input.rs
@@ -77,7 +77,7 @@ struct AudioSample(Arc<[Vec<f32>; NUMBER_OF_CHANNELS]>);
 /// Reads audio samples into a temp buffer and returns that buffer.
 fn microphone_input(device: Arc<Mutex<PCM>>) -> Result<AudioSample> {
     let io_device = device.lock().expect("Failed to lock device.");
-    let io = io_device.io_f32().into_diagnostic().expect("Failed to io.");
+    let io = io_device.io_f32().into_diagnostic()?;
 
     let mut interleaved_buffer = [0.0_f32; NUMBER_OF_SAMPLES * NUMBER_OF_CHANNELS];
     let number_of_frames = io.readi(&mut interleaved_buffer).into_diagnostic()?;
