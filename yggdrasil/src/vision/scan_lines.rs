e57c651ddbb50bf0e1d07a48630a55e9cdd86fb8
Aemulation <112875887+Aemulation@users.noreply.github.com>
2024-02-23 13:54:11 +0100

    Remove debug/test code

--- yggdrasil/src/vision/scan_lines.rs
+++ yggdrasil/src/vision/scan_lines.rs
@@ -1,105 +1,11 @@
-// TODO: Remove import after testing phase is over.
-use image::codecs::jpeg::JpegEncoder;
-// TODO: Remove this function after testing phase is over.
-fn yuyv_to_rgb(
-    source: &[u8],
-    len: usize,
-    mut destination: impl Write,
-    rotate_180_degrees: bool,
-) -> Result<()> {
-    fn clamp(value: i32) -> u8 {
-        value.clamp(0, 255) as u8
-    }
-
-    fn yuyv422_to_rgb(y1: u8, u: u8, y2: u8, v: u8) -> ((u8, u8, u8), (u8, u8, u8)) {
-        let y1 = i32::from(y1) - 16;
-        let u = i32::from(u) - 128;
-        let y2 = i32::from(y2) - 16;
-        let v = i32::from(v) - 128;
-
-        let red1 = (298 * y1 + 409 * v + 128) >> 8;
-        let green1 = (298 * y1 - 100 * u - 208 * v + 128) >> 8;
-        let blue1 = (298 * y1 + 516 * u + 128) >> 8;
-
-        let red2 = (298 * y2 + 409 * v + 128) >> 8;
-        let green2 = (298 * y2 - 100 * u - 208 * v + 128) >> 8;
-        let blue2 = (298 * y2 + 516 * u + 128) >> 8;
-
-        (
-            (clamp(red1), clamp(green1), clamp(blue1)),
-            (clamp(red2), clamp(green2), clamp(blue2)),
-        )
-    }
-
-    // Two pixels are stored in four bytes. Those four bytes are the y1, u, y2, v values in
-    // that order. Because two pixels share the same u and v value, we decode both pixels at
-    // the same time (using `yuyv422_to_rgb`), instead of one-by-one, to improve performance.
-    //
-    // A `pixel_duo` here refers to the two pixels with the same u and v values.
-    // We iterate over all the pixel duo's in `source`, which is why we take steps of four
-    // bytes.
-    // for pixel_duo_id in 0..(source.len() / 4) {
-    for pixel_duo_id in 0..len / 2 {
-        let input_offset: usize = if rotate_180_degrees {
-            source.len() - 4 * pixel_duo_id - 4
-        } else {
-            pixel_duo_id * 4
-        };
-
-        let y1 = source[input_offset];
-        let u = source[input_offset + 1];
-        let y2 = source[input_offset + 2];
-        let v = source[input_offset + 3];
-
-        let ((red1, green1, blue1), (red2, green2, blue2)) = yuyv422_to_rgb(y1, u, y2, v);
-
-        if rotate_180_degrees {
-            destination
-                .write_all(&[red2, green2, blue2, red1, green1, blue1])
-                .unwrap();
-        } else {
-            destination
-                .write_all(&[red1, green1, blue1, red2, green2, blue2])
-                .unwrap();
-        }
-    }
-
-    Ok(())
-}
-
-// TODO: Remove this function after testing phase is over.
-pub fn store_jpeg(
-    image: Vec<u8>,
-    width: usize,
-    height: usize,
-    file_path: impl AsRef<Path>,
-) -> Result<()> {
-    let output_file = File::create(file_path).unwrap();
-    let mut encoder = JpegEncoder::new(output_file);
-
-    let mut rgb_buffer = Vec::<u8>::with_capacity(width * height * 3);
-
-    yuyv_to_rgb(&image, width * height, &mut rgb_buffer, false)?;
-
-    encoder
-        .encode(
-            &rgb_buffer,
-            u32::try_from(width).unwrap(),
-            u32::try_from(height).unwrap(),
-            image::ColorType::Rgb8,
-        )
-        .unwrap();
-
-    Ok(())
-}
-use std::{fs::File, io::Write, ops::Deref, path::Path, process::exit, time::Instant};
-
 use crate::{
     camera::{BottomImage, Image, TopImage},
     prelude::*,
 };
 
 use heimdall::YuyvImage;
+
+use std::ops::Deref;
 
 pub struct ScanLinesModule;
 
@@ -232,7 +138,6 @@ impl PixelColor {
         // TODO: Find a better way to classify pixels.
         if y1 > 140 {
             Self::White
-        // } else if (y1 > 65) && (u > 90) && (u < 110) && (v > 90) && (v < 135) {
         } else if (y1 > 45) && (u > 70) && (u < 160) && (v > 70) && (v < 160) {
             Self::Green
         } else if (y1 < 50) && (u > 110) && (u < 150) && (v > 110) && (v < 150) {
@@ -264,7 +169,6 @@ fn make_horizontal_ids(image: &Image) -> Vec<usize> {
         }
     }
     for row_id in image.yuyv_image().height() / 4..image.yuyv_image().height() / 2 {
-        // if row_id % 12 == 0 {
         if row_id % 8 == 0 {
             horizontal_ids.push(row_id);
         }
@@ -391,15 +295,9 @@ fn vertical_scan_lines(yuyv_image: &YuyvImage, scan_lines: &mut ScanLines) {
 }
 
 fn update_scan_lines(image: &Image, scan_lines: &mut ScanLines) {
-    let horizontal_start = Instant::now();
     horizontal_scan_lines(image.yuyv_image(), scan_lines);
-    // TODO: Remove this debug print.
-    eprintln!("horizontal elapsed: {:?}", horizontal_start.elapsed());
 
-    let vertical_start = Instant::now();
     vertical_scan_lines(image.yuyv_image(), scan_lines);
-    // TODO: Remove this debug print.
-    eprintln!("vertical elapsed: {:?}", vertical_start.elapsed());
 }
 
 #[system]
@@ -420,65 +318,5 @@ pub fn scan_lines_system(
 
         bottom_scan_lines.scan_lines.image = bottom_image.deref().clone();
     }
-
-    // TODO: Remove this. This stores the horizontal scan lines as a jpeg.
-    {
-        let mut row_yuyv_buffer = vec![0u8; top_scan_lines.width() * top_scan_lines.height() * 2];
-        for (horizontal_id, row_id) in top_scan_lines.row_ids().iter().enumerate() {
-            let row = top_scan_lines.horizontal_line(horizontal_id);
-
-            let offset = row_id * top_scan_lines.width() * 2;
-
-            for pixel_duo in 0..top_image.yuyv_image().width() / 2 {
-                let yuyv_pixel_duo = match row[pixel_duo * 2] {
-                    PixelColor::White => [128u8, 255u8, 128u8, 255u8],
-                    PixelColor::Black => [128u8, 0u8, 128u8, 255u8],
-                    PixelColor::Green => [128u8, 255u8, 128u8, 0u8],
-                    PixelColor::Unknown => [0u8, 0u8, 0u8, 0u8],
-                };
-
-                row_yuyv_buffer.as_mut_slice()[offset + pixel_duo * 4..offset + pixel_duo * 4 + 4]
-                    .copy_from_slice(&yuyv_pixel_duo);
-            }
-        }
-        store_jpeg(
-            row_yuyv_buffer,
-            top_scan_lines.width(),
-            top_scan_lines.height(),
-            "yggdrasil_row_image.jpeg",
-        )?;
-    }
-
-    // TODO: Remove this. This stores the vertical scan lines as a jpeg.
-    {
-        let mut col_yuyv_buffer = vec![0u8; top_scan_lines.width() * top_scan_lines.height() * 2];
-        for (vertical_id, col_id) in top_scan_lines.column_ids().iter().enumerate() {
-            let col = top_scan_lines.vertical_line(vertical_id);
-
-            for (row_id, pixel) in col.iter().enumerate() {
-                let buffer_offset = (row_id * top_scan_lines.width() + col_id) * 2;
-
-                let yuyv_pixel_duo = match pixel {
-                    PixelColor::White => [128u8, 255u8, 128u8, 255u8],
-                    PixelColor::Black => [128u8, 0u8, 128u8, 255u8],
-                    PixelColor::Green => [128u8, 255u8, 128u8, 0u8],
-                    PixelColor::Unknown => [0u8, 0u8, 0u8, 0u8],
-                };
-
-                col_yuyv_buffer.as_mut_slice()[buffer_offset..buffer_offset + 4]
-                    .copy_from_slice(&yuyv_pixel_duo);
-            }
-        }
-        store_jpeg(
-            col_yuyv_buffer,
-            top_scan_lines.width(),
-            top_scan_lines.height(),
-            "yggdrasil_col_image.jpeg",
-        )?;
-    }
-
-    // TODO: Remove this.
-    exit(0);
-
     Ok(())
 }
