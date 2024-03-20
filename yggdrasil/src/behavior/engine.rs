73127806d28fa181cddac237e202a540c58fb76f
Jakob Kaiser <jakob.j.jeep@gmail.com>
2024-03-20 02:06:23 +0100

    added config for initial robot position

--- yggdrasil/src/behavior/engine.rs
+++ yggdrasil/src/behavior/engine.rs
@@ -86,8 +86,7 @@ pub enum BehaviorKind {
 
 impl Default for BehaviorKind {
     fn default() -> Self {
-        // BehaviorKind::Passive(Passive)
-        BehaviorKind::Initial(Initial)
+        BehaviorKind::Passive(Passive)
     }
 }
 
