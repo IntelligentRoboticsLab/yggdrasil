7f08a976c755a5701ab92dd4058a433c842f38bb
Aemulation <112875887+Aemulation@users.noreply.github.com>
2024-03-22 23:42:33 +0100

    Call `assert_valid_bin` when running `sindri update`

--- crates/sindri/src/cli/update.rs
+++ crates/sindri/src/cli/update.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use miette::{IntoDiagnostic, Result};
+use miette::{miette, IntoDiagnostic, Result};
 
 #[derive(Parser, Debug)]
 /// Compile, deploy and run the specified binary to the robot.
@@ -7,6 +7,9 @@ pub struct Update {}
 
 impl Update {
     pub async fn update(self) -> Result<()> {
+        crate::cargo::assert_valid_bin("sindri")
+            .map_err(|_| miette!("Command must be executed from the yggdrasil directory"))?;
+
         tokio::process::Command::new("cargo")
             .args(["install", "--locked", "--path", "crates/sindri"])
             .status()
