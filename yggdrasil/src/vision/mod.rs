642e4c955e504b412fb19bc64892888e23b80316
Harold Ruiter <harold@soaratorium.com>
2024-03-22 13:37:56 +0100

    Add field boundary detection

--- yggdrasil/src/vision/mod.rs
+++ yggdrasil/src/vision/mod.rs
@@ -2,9 +2,11 @@ use crate::prelude::*;
 
 use serde::{Deserialize, Serialize};
 
+pub mod field_boundary;
 pub mod line_detection;
 pub mod scan_lines;
 
+use field_boundary::FieldBoundaryModule;
 use line_detection::LineDetectionModule;
 use scan_lines::{ScanLinesConfig, ScanLinesModule};
 
@@ -13,6 +15,7 @@ pub struct VisionModule;
 impl Module for VisionModule {
     fn initialize(self, app: App) -> Result<App> {
         app.add_module(ScanLinesModule)?
+            .add_module(FieldBoundaryModule)?
             .add_module(LineDetectionModule)
     }
 }
