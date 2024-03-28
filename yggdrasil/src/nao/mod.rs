c12bdf0c1273f0e0d2cbaecd93eabee6ceab75e8
Aemulation <112875887+Aemulation@users.noreply.github.com>
2024-03-28 21:35:10 +0100

    Introduce the `NaoArbiter`

--- yggdrasil/src/nao/mod.rs
+++ yggdrasil/src/nao/mod.rs
@@ -1,6 +1,8 @@
 mod cycle;
 pub use cycle::*;
 
+pub mod arbiter;
+
 use crate::prelude::*;
 use std::{env, time::Duration};
 
@@ -70,7 +72,8 @@ impl Module for NaoModule {
         Ok(app
             .add_startup_system(initialize_nao)?
             .init_resource::<NaoControlMessage>()?
-            .add_system(write_hardware_info)
+            .add_module(arbiter::NaoArbiterModule)?
+            .add_system(write_hardware_info.after(arbiter::finalize))
             .add_startup_system(cycle::initialize_cycle_counter)?
             .add_system(cycle::update_cycle_stats.after(write_hardware_info)))
     }
