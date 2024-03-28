751ae14c4a64f3d446775515dae4f39c32874948
Gijs de Jong <berichtaangijs@gmail.com>
2024-03-28 21:03:33 +0100

    Add docs to low pass filter constructor

--- yggdrasil/src/filter/low_pass_filter.rs
+++ yggdrasil/src/filter/low_pass_filter.rs
@@ -1,6 +1,6 @@
 use std::ops::{Add, Mul};
 
-#[derive(Default)]
+#[derive(Default, Clone, Copy)]
 pub struct LowPassFilter<T: Default + Clone + Copy + Add<Output = T> + Mul<Output = T>> {
     pub state: T,
     params: (T, T),
@@ -10,12 +10,19 @@ impl<T> LowPassFilter<T>
 where
     T: Default + Clone + Copy + Add<Output = T> + Mul<Output = T>,
 {
-    pub fn new(initial: T, params: (T, T)) -> Self {
+    /// Create a new [`LowPassFilter`].
+    ///
+    /// The state in this filter will be updated according to this formula:
+    /// ```ignore
+    /// new_state = high * old + low * new
+    /// ```
+    pub fn new(initial: T, high: T, low: T) -> Self {
         LowPassFilter {
             state: initial,
-            params,
+            params: (high, low),
         }
     }
+
     /// Update the current state of this [`LowPassFilter`] using the new value.
     pub fn update(&mut self, value: T) {
         self.state = self.params.0 * self.state + self.params.1 * value;
@@ -28,7 +35,7 @@ mod tests {
 
     #[test]
     fn update() {
-        let mut filter = LowPassFilter::new(0.0, (0.8, 0.2));
+        let mut filter = LowPassFilter::new(0.0, 0.8, 0.2);
         assert_eq!(filter.state, 0.0);
 
         filter.update(0.5);
