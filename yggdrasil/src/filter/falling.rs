04d2be8c2bb1687a8fc25ac3855772bd488bc945
Stephan <93551909+JinseiSteven@users.noreply.github.com>
2024-04-05 17:14:47 +0200

    Fall Filter (#76)
    Co-authored-by: Joost Weerheim <weerheimjoost2000@gmail.com>
    Co-authored-by: Harold <37922636+YukumoHunter@users.noreply.github.com>
    Co-authored-by: Colin de Koning <colinuithem@gmail.com>
    Co-authored-by: Aemulation <112875887+Aemulation@users.noreply.github.com>

---
+++ yggdrasil/src/filter/falling.rs
@@ -0,0 +1,131 @@
+use crate::filter::{fsr::Contacts, imu::IMUValues};
+use miette::Result;
+use tyr::prelude::*;
+
+/// Maximum angle for standing upright.
+const MAX_UPRIGHT_ANGLE: f32 = 0.1;
+/// Minimum angle for falling detection.
+const MIN_FALL_ANGLE_FORWARDS: f32 = 0.45;
+const MIN_FALL_ANGLE_BACKWARDS: f32 = -0.45;
+const MIN_FALL_ANGLE_LEFT: f32 = -0.52;
+const MIN_FALL_ANGLE_RIGHT: f32 = 0.52;
+/// Minimum velocity for falling detection.
+const MIN_FALL_VELOCITY_FORWARDS: f32 = 0.15;
+const MIN_FALL_VELOCITY_BACKWARDS: f32 = 0.15;
+const MIN_FALL_VELOCITY_LEFT: f32 = 0.15;
+const MIN_FALL_VELOCITY_RIGHT: f32 = 0.15;
+// Minimum angle for lying confirmation.
+const MIN_LYING_ANGLE: f32 = 1.5;
+/// Minimum accelerometer deviation for lying confirmation.
+const MAX_ACC_DEVIATION: f32 = 0.175;
+
+/// A module offering a Pose resource, containing the current pose state of the robot, and rudimentary falling detection.
+///
+/// This module provides the following resources to the application:
+/// - [`Fall`]
+pub struct FallingFilter;
+
+impl Module for FallingFilter {
+    fn initialize(self, app: App) -> Result<App> {
+        app.add_system(pose_filter).init_resource::<Fall>()
+    }
+}
+
+/// Struct containing the current FallState of the NAO.
+#[derive(Default)]
+pub struct Fall {
+    pub state: FallState,
+}
+
+/// FallState contains the variants: Falling, Upright and Lying. Both Falling and Lying have their
+/// associated values which are again, enum types containing the directions the robot can fall or
+/// lie in.
+#[derive(Default, Clone, Debug)]
+pub enum FallState {
+    Falling(FallDirection),
+    #[default]
+    Upright,
+    Lying(LyingDirection),
+}
+
+/// FallDirection contains four variants which are associated with the direction of the fall.
+#[derive(Clone, Debug)]
+pub enum FallDirection {
+    Forwards,
+    Backwards,
+    Leftways,
+    Rightways,
+}
+
+/// LyingDirection contains two variants which are associated with the position of a fallen robot.
+#[derive(Clone, Debug)]
+pub enum LyingDirection {
+    FacingUp,
+    FacingDown,
+}
+
+/// Is the robot falling forward based on its angle and gyroscope.
+fn is_falling_forward(imu_values: &IMUValues) -> bool {
+    (imu_values.angles.y > MIN_FALL_ANGLE_FORWARDS)
+        && imu_values.gyroscope.y.abs() > MIN_FALL_VELOCITY_FORWARDS
+}
+
+/// Is the robot falling backwards based on its angle and gyroscope.
+fn is_falling_backward(imu_values: &IMUValues) -> bool {
+    (imu_values.angles.y < MIN_FALL_ANGLE_BACKWARDS)
+        && imu_values.gyroscope.y.abs() > MIN_FALL_VELOCITY_BACKWARDS
+}
+
+/// Is the robot falling left based on its angle and gyroscope.
+fn is_falling_left(imu_values: &IMUValues) -> bool {
+    (imu_values.angles.x < MIN_FALL_ANGLE_LEFT)
+        && imu_values.gyroscope.x.abs() > MIN_FALL_VELOCITY_LEFT
+}
+
+/// Is the robot falling right based on its angle and gyroscope.
+fn is_falling_right(imu_values: &IMUValues) -> bool {
+    (imu_values.angles.x > MIN_FALL_ANGLE_RIGHT)
+        && imu_values.gyroscope.x.abs() > MIN_FALL_VELOCITY_RIGHT
+}
+
+/// Is the robot standing upright based on its angles and ground contact.
+fn is_standing_upright(imu_values: &IMUValues, contacts: &Contacts) -> bool {
+    imu_values.angles.x < MAX_UPRIGHT_ANGLE
+        && imu_values.angles.y < MAX_UPRIGHT_ANGLE
+        && contacts.ground
+}
+
+/// Is the robot lying on its stomach based on the accelerometer and angle.
+fn is_lying_on_stomach(imu_values: &IMUValues) -> bool {
+    imu_values.accelerometer_variance.y < MAX_ACC_DEVIATION
+        && imu_values.angles.y >= MIN_LYING_ANGLE
+}
+
+/// Is the robot lying on its back based on the accelerometer and angle.
+fn is_lying_on_back(imu_values: &IMUValues) -> bool {
+    imu_values.accelerometer_variance.y < MAX_ACC_DEVIATION
+        && imu_values.angles.y <= -MIN_LYING_ANGLE
+}
+
+/// Checks position of the robot and sets [`FallState`], [`FallDirection`] and [`LyingDirection`]
+/// accordingly.
+#[system]
+fn pose_filter(imu_values: &IMUValues, fallingstate: &mut Fall, contacts: &Contacts) -> Result<()> {
+    if is_falling_forward(imu_values) {
+        fallingstate.state = FallState::Falling(FallDirection::Forwards);
+    } else if is_falling_backward(imu_values) {
+        fallingstate.state = FallState::Falling(FallDirection::Backwards);
+    } else if is_falling_left(imu_values) {
+        fallingstate.state = FallState::Falling(FallDirection::Leftways);
+    } else if is_falling_right(imu_values) {
+        fallingstate.state = FallState::Falling(FallDirection::Rightways);
+    } else if is_standing_upright(imu_values, contacts) {
+        fallingstate.state = FallState::Upright;
+    } else if is_lying_on_stomach(imu_values) {
+        fallingstate.state = FallState::Lying(LyingDirection::FacingDown);
+    } else if is_lying_on_back(imu_values) {
+        fallingstate.state = FallState::Lying(LyingDirection::FacingUp);
+    }
+
+    Ok(())
+}
--- yggdrasil/src/filter/imu.rs
+++ yggdrasil/src/filter/imu.rs
@@ -1,5 +1,13 @@
 use crate::prelude::*;
+use miette::Result;
+use nidhogg::types::FillExt;
 use nidhogg::{types::Vector2, types::Vector3, NaoState};
+use num::traits::pow::Pow;
+use std::collections::VecDeque;
+use std::ops::Div;
+
+/// Amount of accelerometer measurements to calculate standard deviation over
+const ACCELEROMETER_DEVIATION_WINDOW: usize = 50;
 
 /// A module offering a structured wrapper for the parts of the IMU, derived from the raw [`NaoState`].
 ///
@@ -14,7 +22,6 @@ impl Module for IMUFilter {
 }
 
 /// Struct containing gyroscope, accelerometer and angles.
-#[derive(Default, Clone)]
 pub struct IMUValues {
     /// The Gyroscope provides direct measurements of the rotational speed along
     /// three axes (x, y and z) in radians per second (rad/s). The Z axis is facing up.
@@ -30,6 +37,41 @@ pub struct IMUValues {
     ///
     /// These angles represent the orientation of the robot and are measured in radians.
     pub angles: Vector2<f32>,
+
+    pub accelerometer_variance: Vector3<f32>,
+
+    accelerometer_measurements: VecDeque<Vector3<f32>>,
+}
+
+impl Default for IMUValues {
+    fn default() -> Self {
+        let mut accelerometer_measurements =
+            VecDeque::with_capacity(ACCELEROMETER_DEVIATION_WINDOW);
+        for _ in 0..ACCELEROMETER_DEVIATION_WINDOW {
+            accelerometer_measurements.push_back(Vector3::default());
+        }
+
+        IMUValues {
+            gyroscope: Vector3::default(),
+            accelerometer: Vector3::default(),
+            angles: Vector2::default(),
+            accelerometer_variance: Vector3::default(),
+            accelerometer_measurements,
+        }
+    }
+}
+
+/// Calculate the variance of `measurements`, multiplied by `ACCELEROMETER_DEVIATION_WINDOW`.
+fn variance(measurements: &VecDeque<Vector3<f32>>) -> Vector3<f32> {
+    let measurement_avg: Vector3<f32> = measurements
+        .iter()
+        .sum::<Vector3<f32>>()
+        .div(Vector3::fill(measurements.len() as f32));
+
+    measurements.iter().fold(Vector3::default(), |acc, item| {
+        let diff: Vector3<f32> = measurement_avg - item;
+        acc + diff.pow(2)
+    })
 }
 
 #[system]
@@ -37,6 +79,13 @@ pub fn imu_filter(nao_state: &NaoState, imu_values: &mut IMUValues) -> Result<()
     imu_values.gyroscope = nao_state.gyroscope;
     imu_values.accelerometer = nao_state.accelerometer;
     imu_values.angles = nao_state.angles;
+
+    imu_values.accelerometer_measurements.pop_front();
+    imu_values
+        .accelerometer_measurements
+        .push_back(nao_state.accelerometer);
+
+    imu_values.accelerometer_variance = variance(&imu_values.accelerometer_measurements);
 
     Ok(())
 }
