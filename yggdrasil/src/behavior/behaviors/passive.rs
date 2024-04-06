f7876442bb5a9d51ba3bbf20c5b95f555d20c68c
Jakob Kaiser <jakob.j.jeep@gmail.com>
2024-04-06 21:55:07 +0200

    wip

--- yggdrasil/src/behavior/behaviors/passive.rs
+++ yggdrasil/src/behavior/behaviors/passive.rs
@@ -2,10 +2,7 @@ use crate::{
     behavior::engine::{Behavior, Context},
     nao::manager::{NaoManager, Priority},
 };
-use nidhogg::types::{
-    color, ArmJoints, FillExt, HeadJoints, JointArray, LeftArmJoints, LeftLegJoints, LegJoints,
-    RightArmJoints, RightEye, RightLegJoints,
-};
+use nidhogg::types::{color, ArmJoints, FillExt, HeadJoints, JointArray, LegJoints, RightEye};
 
 const DEFAULT_PASSIVE_STIFFNESS: f32 = 0.8;
 const DEFAULT_PASSIVE_PRIORITY: Priority = Priority::Medium;
@@ -46,83 +43,31 @@ fn set_initial_joint_values(
     initial_joint_positions: JointArray<f32>,
     nao_manager: &mut NaoManager,
 ) {
-    let JointArray {
-        head_yaw,
-        head_pitch,
-        left_shoulder_pitch,
-        left_shoulder_roll,
-        left_elbow_yaw,
-        left_elbow_roll,
-        left_wrist_yaw,
-        left_hip_yaw_pitch,
-        left_hip_roll,
-        left_hip_pitch,
-        left_knee_pitch,
-        left_ankle_pitch,
-        left_ankle_roll,
-        right_shoulder_pitch,
-        right_shoulder_roll,
-        right_elbow_yaw,
-        right_elbow_roll,
-        right_wrist_yaw,
-        right_hip_roll,
-        right_hip_pitch,
-        right_knee_pitch,
-        right_ankle_pitch,
-        right_ankle_roll,
-        left_hand,
-        right_hand,
-    } = initial_joint_positions;
+    let head_joints = initial_joint_positions.head_joints();
     nao_manager.set_head(
-        HeadJoints {
-            yaw: head_yaw,
-            pitch: head_pitch,
-        },
+        head_joints,
         HeadJoints::fill(DEFAULT_PASSIVE_STIFFNESS),
         DEFAULT_PASSIVE_PRIORITY,
     );
 
+    let left_arm_joints = initial_joint_positions.left_arm_joints();
+    let right_arm_joints = initial_joint_positions.right_arm_joints();
     nao_manager.set_arms(
-        ArmJoints {
-            left_arm: LeftArmJoints {
-                shoulder_pitch: left_shoulder_pitch,
-                shoulder_roll: left_shoulder_roll,
-                elbow_yaw: left_elbow_yaw,
-                elbow_roll: left_elbow_roll,
-                wrist_yaw: left_wrist_yaw,
-                hand: left_hand,
-            },
-            right_arm: RightArmJoints {
-                shoulder_pitch: right_shoulder_pitch,
-                shoulder_roll: right_shoulder_roll,
-                elbow_yaw: right_elbow_yaw,
-                elbow_roll: right_elbow_roll,
-                wrist_yaw: right_wrist_yaw,
-                hand: right_hand,
-            },
-        },
+        ArmJoints::builder()
+            .left_arm(left_arm_joints)
+            .right_arm(right_arm_joints)
+            .build(),
         ArmJoints::fill(DEFAULT_PASSIVE_STIFFNESS),
         DEFAULT_PASSIVE_PRIORITY,
     );
 
+    let left_leg_joints = initial_joint_positions.left_leg_joints();
+    let right_leg_joints = initial_joint_positions.right_leg_joints();
     nao_manager.set_legs(
-        LegJoints {
-            left_leg: LeftLegJoints {
-                hip_yaw_pitch: left_hip_yaw_pitch,
-                hip_roll: left_hip_roll,
-                hip_pitch: left_hip_pitch,
-                knee_pitch: left_knee_pitch,
-                ankle_pitch: left_ankle_pitch,
-                ankle_roll: left_ankle_roll,
-            },
-            right_leg: RightLegJoints {
-                hip_roll: right_hip_roll,
-                hip_pitch: right_hip_pitch,
-                knee_pitch: right_knee_pitch,
-                ankle_pitch: right_ankle_pitch,
-                ankle_roll: right_ankle_roll,
-            },
-        },
+        LegJoints::builder()
+            .left_leg(left_leg_joints)
+            .right_leg(right_leg_joints)
+            .build(),
         LegJoints::fill(DEFAULT_PASSIVE_STIFFNESS),
         DEFAULT_PASSIVE_PRIORITY,
     );
