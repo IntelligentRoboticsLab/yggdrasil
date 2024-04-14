889671858a972dc01c1b7e964e3e052a43413fbb
Aemulation <112875887+Aemulation@users.noreply.github.com>
2024-04-14 11:44:25 +0200

    Cleanup + straght walk improvements

--- yggdrasil/src/motion/step_planning.rs
+++ yggdrasil/src/motion/step_planning.rs
@@ -16,7 +16,7 @@ pub struct WalkPlannerModule;
 impl Module for WalkPlannerModule {
     fn initialize(self, app: App) -> Result<App> {
         let target = WalkPlannerTarget {
-            target_position: Point2::new(0., 1.),
+            target_position: Point2::new(-1., -1.),
         };
 
         app.add_system(walk_planner_system)
@@ -63,34 +63,25 @@ fn walk_planner_system(
         return Ok(());
     }
 
-    // let point = odometry
-    //     .accumulated
-    //     .rotation
-    //     .transform_point(&Point2::new(0.1, 0.0));
-    // let point = Vector2::new(point.x, point.y);
-    // eprintln!("point: {point}");
-
     let target_vector = Vector2::new(
         walk_planner_target.target_position.x,
         walk_planner_target.target_position.y,
     );
-
     let target_vector = odometry.accumulated.transform_vector(&target_vector);
 
-    // eprintln!("target vector: {target_vector}");
-    // eprintln!("translation: {}", odometry.accumulated.translation);
-
-    let angle = odometry
+    let robot_target_angle = odometry
         .accumulated
         .inverse()
         .transform_vector(&Vector2::new(0.1, 0.0))
         .angle(&target_vector);
-    if angle == NAN {
+    if robot_target_angle == NAN {
         exit(0);
     }
-
-    // eprintln!("angle: {}", angle / (2. * std::f32::consts::PI) * 360.);
-    // eprintln!("angle: {angle}");
+    let turn = calc_turn(
+        &odometry.accumulated.translation,
+        robot_rotation.yaw().angle(),
+        &walk_planner_target.target_position,
+    );
 
     let robot_point: Point2<f32> = Point2::new(
         odometry.accumulated.translation.x,
@@ -102,35 +93,21 @@ fn walk_planner_system(
         walk_planner_target.target_position.y,
     );
 
-    // eprintln!("target: {target_vector}");
-    // eprintln!("point:  {robot_point}");
-    // let distance = (target_point - robot_point).norm();
     let distance = distance(&target_point, &robot_point);
-
-    eprintln!("distance: {distance}");
-    eprintln!("angle: {angle}");
 
     if distance < 0.1 {
         walking_engine.request_idle();
-    } else if angle > 0.1 {
-        let turn = calc_turn(
-            &odometry.accumulated.translation,
-            robot_rotation.yaw().angle(),
-            &walk_planner_target.target_position,
-        );
-        eprintln!("turn: {turn}");
-
+    } else if robot_target_angle > 0.1 {
         walking_engine.request_walk(Step {
             forward: 0.,
             left: 0.,
             turn,
         });
     } else {
-        eprintln!("FORWARD");
         walking_engine.request_walk(Step {
             forward: 0.03,
             left: 0.,
-            turn: 0.,
+            turn,
         });
     }
 
