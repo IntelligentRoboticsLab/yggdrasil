0e0e00a17c24e8cc3e8bcbf69b1396ac6029af16
Fyor <fyorkg@gmail.com>
2024-04-19 16:00:40 +0200

    get_best_ball

--- yggdrasil/src/vision/ball_detection/classifier.rs
+++ yggdrasil/src/vision/ball_detection/classifier.rs
@@ -67,10 +67,7 @@ pub struct Ball {
     pub robot_to_ball: Vector2<f32>,
     pub distance: f32,
     pub timestamp: Instant,
-<<<<<<< HEAD
     pub confidence: f32,
-=======
->>>>>>> 5f9dc18 (Ball life extension)
 }
 
 #[derive(Clone)]
@@ -153,10 +150,7 @@ pub(super) fn detect_balls(
                             robot_to_ball: robot_to_ball.xy().coords,
                             distance: proposal.distance_to_ball,
                             timestamp: Instant::now(),
-<<<<<<< HEAD
                             confidence,
-=======
->>>>>>> 5f9dc18 (Ball life extension)
                         });
                     }
                 }
