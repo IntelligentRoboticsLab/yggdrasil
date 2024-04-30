7c92ae5b5c3b486dc133ded0093f619cb10e1885
J.S. Blaauboer <j.s.blaauboer@gmail.com>
2024-04-30 16:56:34 +0200

    broadcast: Add unit tests

--- crates/bifrost/src/broadcast/buffer.rs
+++ crates/bifrost/src/broadcast/buffer.rs
@@ -256,3 +256,103 @@ impl<M: Message> Fragment<M> {
         self.data.len()
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// Unit tests
+////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use std::io::{Read, Write};
+    use std::ops::Add;
+
+    use crate::{Result, serialization::{Encode, Decode}};
+    use super::*;
+
+    /// Dummy message that encodes to `n` bytes with value `n`.
+    struct Dummy(u8);
+
+    impl Encode for Dummy {
+        fn encode(&self, mut write: impl Write) -> Result<()> {
+            Ok(write.write_all(&vec![self.0; self.encode_len()])?)
+        }
+
+        fn encode_len(&self) -> usize {
+            self.0 as usize
+        }
+    }
+
+    impl Decode for Dummy {
+        fn decode(mut read: impl Read) -> Result<Self> {
+            let mut buf = [0; Self::MAX_PACKET_SIZE];
+            read.read_exact(&mut buf[..1])?;
+            let n = buf[0] as usize;
+            read.read_exact(&mut buf[1..n])?;
+            Ok(Self(buf[0]))
+        }
+    }
+
+    impl Message for Dummy {
+        const MAX_PACKET_SIZE: usize = 8;
+        const EXPECTED_SIZE: usize = 8;
+    }
+
+    /// Helper type for quickly constructing timestamps.
+    #[derive(Clone, Copy)]
+    struct Epoch(Instant);
+
+    impl Add<isize> for Epoch {
+        type Output = Instant;
+
+        fn add(self, rhs: isize) -> Instant {
+            if rhs >= 0 {
+                self.0 + Duration::from_secs(rhs as u64)
+            } else {
+                self.0 - Duration::from_secs(-rhs as u64)
+            }
+        }
+    }
+
+    #[test]
+    fn test_buffer() {
+        let rate = Rate {
+            late_threshold: Duration::ZERO,
+            automatic_deadline: Duration::from_secs(5),
+            early_threshold: Duration::from_secs(10),
+            dead_space: 2,
+        };
+
+        let mut buffer = Buffer::new(rate);
+        let t = Epoch(Instant::now());
+
+        buffer.push_at(Dummy(7), Deadline::Automatic, t+0).unwrap();
+        buffer.push_at(Dummy(4), Deadline::Automatic, t+1).unwrap();
+        buffer.push_at(Dummy(6), Deadline::Automatic, t+2).unwrap();
+        buffer.push_at(Dummy(3), Deadline::Automatic, t+3).unwrap();
+        buffer.push_at(Dummy(1), Deadline::ASAP, t+4).unwrap();
+
+        assert_eq!(buffer.pack_at(t+5), Some(vec![1,7,7,7,7,7,7,7]));
+        assert_eq!(buffer.pack_at(t+6), Some(vec![4,4,4,4,3,3,3]));
+        assert_eq!(buffer.pack_at(t+7), Some(vec![6,6,6,6,6,6]));
+    }
+
+    #[test]
+    fn test_buffer_early() {
+        let rate = Rate {
+            late_threshold: Duration::ZERO,
+            automatic_deadline: Duration::ZERO,
+            early_threshold: Duration::ZERO,
+            dead_space: 2,
+        };
+
+        let mut buffer = Buffer::new(rate);
+        let t = Epoch(Instant::now());
+
+        buffer.push_at(Dummy(3), Deadline::WHENEVER, t+0).unwrap();
+        assert_eq!(buffer.pack_at(t+0), None);
+        buffer.push_at(Dummy(2), Deadline::WHENEVER, t+1).unwrap();
+        assert_eq!(buffer.pack_at(t+1), None);
+        buffer.push_at(Dummy(1), Deadline::WHENEVER, t+2).unwrap();
+        assert_eq!(buffer.pack_at(t+2), Some(vec![3,3,3,2,2,1]));
+    }
+}
