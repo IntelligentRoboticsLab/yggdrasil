0bfa6241fa89f12e0ec125eb7270b597e76143ea
Aemulation <112875887+Aemulation@users.noreply.github.com>
2024-07-11 13:56:51 +0200

    Send correct data to game-controller

--- yggdrasil/src/game_controller/transmit.rs
+++ yggdrasil/src/game_controller/transmit.rs
@@ -16,8 +16,13 @@ use std::time::{Duration, Instant};
 use miette::IntoDiagnostic;
 
 use crate::core::config::showtime::PlayerConfig;
+use crate::localization::RobotPose;
 use crate::prelude::*;
 use crate::sensor::falling::FallState;
+use crate::vision::ball_detection::classifier::Balls;
+
+const NO_BALL_DETECTED_DATA: (f32, [f32; 2]) = (-1.0, [0.0, 0.0]);
+const MILIMETERS_PER_METER: f32 = 1_000.;
 
 pub(super) struct GameControllerTransmitModule;
 
@@ -72,6 +77,26 @@ async fn transmit_game_controller_return_message(
     Ok((game_controller_message, Instant::now()))
 }
 
+fn robot_pose_to_game_controller_pose(robot_pose: &RobotPose) -> [f32; 3] {
+    let robot_world_position = robot_pose.world_position();
+    [
+        robot_world_position.x * MILIMETERS_PER_METER,
+        robot_world_position.y * MILIMETERS_PER_METER,
+        robot_pose.world_rotation(),
+    ]
+}
+
+fn balls_to_game_controller_ball(balls: &Balls) -> (f32, [f32; 2]) {
+    let Some(ball) = balls.most_confident_ball() else {
+        return NO_BALL_DETECTED_DATA;
+    };
+
+    (
+        ball.timestamp.elapsed().as_secs_f32(),
+        [ball.robot_to_ball.x, ball.robot_to_ball.y],
+    )
+}
+
 #[system]
 fn transmit_system(
     game_controller_data: &mut GameControllerData,
@@ -81,6 +106,8 @@ fn transmit_system(
     fall_state: &FallState,
     game_controller_config: &GameControllerConfig,
     player_config: &PlayerConfig,
+    robot_pose: &RobotPose,
+    balls: &Balls,
 ) -> Result<()> {
     let Some((game_controller_address, mut last_transmitted_update_timestamp)) =
         game_controller_data.game_controller_address
@@ -88,13 +115,15 @@ fn transmit_system(
         return Ok(());
     };
 
+    let game_controller_ball_data = balls_to_game_controller_ball(balls);
+
     let transmit_game_controller_data = TransmitGameControllerData {
         player_num: player_config.player_number,
         team_num: player_config.team_number,
         fallen: matches!(fall_state, FallState::Lying(_)) as u8,
-        pose: [0f32; 3],
-        ball_age: -1.0,
-        ball: [0f32; 2],
+        pose: robot_pose_to_game_controller_pose(robot_pose),
+        ball_age: game_controller_ball_data.0,
+        ball: game_controller_ball_data.1,
     };
 
     match transmit_game_controller_return_message_task.poll() {
