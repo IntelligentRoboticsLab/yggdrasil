02ab2bf39655fa5e47954297754308c3ead305bb
Julia Blaauboer <j.s.blaauboer@gmail.com>
2024-07-12 17:28:42 +0200

    fuckign features

--- yggdrasil/src/vision/camera/mod.rs
+++ yggdrasil/src/vision/camera/mod.rs
@@ -13,7 +13,8 @@ use std::{
 use heimdall::{Camera, CameraDevice, CameraMatrix, ExposureWeights, YuyvImage};
 use matrix::{CalibrationConfig, CameraMatrices};
 
-use super::field_boundary::{FieldBoundary};
+#[cfg(not(feature = "local"))]
+use super::field_boundary::FieldBoundary;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
