2a5f4af372a05dcc1e0cc898d99fc52d0c7798a2
Mark Honkoop <112875887+Aemulation@users.noreply.github.com>
2024-07-17 16:20:35 +0200

    Fix keeper observe behavior (#314)

--- yggdrasil/src/behavior/roles/keeper.rs
+++ yggdrasil/src/behavior/roles/keeper.rs
@@ -5,33 +5,33 @@ use crate::{
         behaviors::{Observe, Walk},
         engine::{BehaviorKind, Context, Control, Role},
     },
-    core::config::layout::FieldConfig,
     motion::step_planner::Target,
 };
 
 pub struct Keeper;
 
-fn is_close_to_own_goal(robot_position: &Point2<f32>, field: &FieldConfig) -> bool {
-    let is_withing_goal_area_length = robot_position.x > field.length / 2.
-        && robot_position.x < field.length / 2. + field.penalty_area_length;
-
-    let is_within_goal_area_width = robot_position.y < field.penalty_area_width / 2.
-        && robot_position.y > -field.penalty_area_width / 2.;
-
-    is_withing_goal_area_length && is_within_goal_area_width
-}
-
 impl Role for Keeper {
-    fn transition_behavior(&mut self, context: Context, _control: &mut Control) -> BehaviorKind {
-        if is_close_to_own_goal(&context.pose.world_position(), &context.layout_config.field) {
-            return BehaviorKind::Observe(Observe::default());
+    fn transition_behavior(&mut self, context: Context, control: &mut Control) -> BehaviorKind {
+        let keeper_target = Target {
+            position: Point2::new(-context.layout_config.field.length / 2., 0.),
+            rotation: Some(UnitComplex::<f32>::from_angle(0.0)),
+        };
+
+        if control
+            .step_planner
+            .current_absolute_target()
+            .is_some_and(|target| target == &keeper_target)
+            || control.step_planner.reached_target()
+        {
+            if let BehaviorKind::Observe(observe) = context.current_behavior {
+                return BehaviorKind::Observe(observe);
+            } else {
+                return BehaviorKind::Observe(Observe::default());
+            };
         }
 
         BehaviorKind::Walk(Walk {
-            target: Target {
-                position: Point2::new(-context.layout_config.field.length / 2., 0.),
-                rotation: Some(UnitComplex::<f32>::from_angle(0.0)),
-            },
+            target: keeper_target,
         })
     }
 }
