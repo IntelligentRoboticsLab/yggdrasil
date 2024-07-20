fe3d0cc967187ab85fe509b4a845155e84c60fa1
Fyor Klein Gunnewiek <fyorkg@gmail.com>
2024-07-20 12:18:55 +0200

    Behaviors to get us scoring

---
+++ yggdrasil/src/behavior/roles/defender.rs
@@ -0,0 +1,45 @@
+use nalgebra::Point2;
+
+use crate::{
+    behavior::{
+        behaviors::{Observe, WalkTo},
+        engine::{BehaviorKind, Context, Control, Role},
+    },
+    motion::step_planner::Target,
+};
+
+#[derive(Debug)]
+pub struct Defender;
+
+impl Role for Defender {
+    fn transition_behavior(&mut self, context: Context, control: &mut Control) -> BehaviorKind {
+        let set_robot_position = context
+            .layout_config
+            .set_positions
+            .player(context.player_config.player_number);
+
+        let set_position = set_robot_position.isometry.translation.vector;
+        let set_point = Point2::new(set_position.x, set_position.y);
+        let defend_target = Target {
+            position: set_point,
+            rotation: Some(set_robot_position.isometry.rotation),
+        };
+
+        if (control.step_planner.has_target() && control.step_planner.reached_target())
+            || (context.pose.distance_to(&set_point) < 0.4
+                && (context.pose.world_rotation() - set_robot_position.isometry.rotation.angle())
+                    .abs()
+                    < 0.2)
+        {
+            if let BehaviorKind::Observe(observe) = context.current_behavior {
+                return BehaviorKind::Observe(observe);
+            } else {
+                return BehaviorKind::Observe(Observe::default());
+            };
+        }
+
+        BehaviorKind::WalkTo(WalkTo {
+            target: defend_target,
+        })
+    }
+}
