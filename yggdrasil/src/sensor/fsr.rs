088c9cd5e7f02f5c8f976c78280a2c6b963beb47
Gijs de Jong <berichtaangijs@gmail.com>
2024-09-10 18:23:01 +0200

    refactor sensor::fsr

--- yggdrasil/src/sensor/fsr.rs
+++ yggdrasil/src/sensor/fsr.rs
@@ -1,44 +1,36 @@
+use super::FilterConfig;
 use crate::prelude::*;
-
+use bevy::prelude::*;
 use nidhogg::{types::ForceSensitiveResistors, NaoState};
 
-use super::FilterConfig;
+/// Plugin offering the Force Sensitive Resistor (FSR) sensor data of the Nao,
+/// derived from the raw [`NaoState`].
+pub struct FSRSensorPlugin;
 
-/// A module offering the Force Sensitive Resistor (FSR) sensor data of the Nao, derived from the raw [`NaoState`].
-///
-/// By allowing systems to depend only on the FSR data, this design enhances the dependency graph's efficiency.
-///
-/// This module provides the following resources to the application:
-/// - [`ForceSensitiveResistors`]
-pub struct FSRSensor;
-
-impl Module for FSRSensor {
-    fn initialize(self, app: App) -> Result<App> {
-        app.add_staged_system(SystemStage::Sensor, force_sensitive_resistor_sensor)
-            .init_resource::<ForceSensitiveResistors>()?
-            .init_resource::<Contacts>()
+impl Plugin for FSRSensorPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Sensor, force_sensitive_resistor_sensor)
+            .init_resource::<ForceSensitiveResistors>()
+            .init_resource::<Contacts>();
     }
 }
 
 /// Struct containing the various contact points of the Nao.
-#[derive(Default)]
+#[derive(Resource, Default)]
 pub struct Contacts {
     /// Whether the Nao is on the ground.
     pub ground: bool,
 }
 
-#[system]
-pub fn force_sensitive_resistor_sensor(
-    nao_state: &NaoState,
-    force_sensitive_resistors: &mut ForceSensitiveResistors,
-    contacts: &mut Contacts,
-    config: &FilterConfig,
-) -> Result<()> {
+fn force_sensitive_resistor_sensor(
+    nao_state: Res<NaoState>,
+    mut force_sensitive_resistors: ResMut<ForceSensitiveResistors>,
+    mut contacts: ResMut<Contacts>,
+    config: Res<FilterConfig>,
+) {
     force_sensitive_resistors.left_foot = nao_state.force_sensitive_resistors.left_foot.clone();
     force_sensitive_resistors.right_foot = nao_state.force_sensitive_resistors.right_foot.clone();
 
     contacts.ground =
         nao_state.force_sensitive_resistors.avg() > config.fsr.ground_contact_threshold;
-
-    Ok(())
 }
