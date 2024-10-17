163958c347a219d10cda0dd4d4a8c41294e666a9
John Yao <yaojohn6@gmail.com>
2024-10-17 13:37:14 +0200

    removing comments

--- yggdrasil/src/localization/mod.rs
+++ yggdrasil/src/localization/mod.rs
@@ -89,8 +89,7 @@ impl RobotPose {
         let y = robot_to_point.y;
         let z = point_in_world.z;
         let yaw = (robot_to_point.y / robot_to_point.x).atan();
-        // let yaw = x.atan2(y);
-        // 0.5 is the height of the robot's primary camera while standing
+        // 0.5 is the height3 of the robot's primary camera while standing
         let pitch = (0.5 - z).atan2((x * x + y * y).sqrt());
 
         HeadJoints { yaw, pitch }
