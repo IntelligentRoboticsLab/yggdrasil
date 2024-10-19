dfabc928aa3ae12dd537019adbb9e8517f4c4cf2
JohnYao93 <yaojohn6@gmail.com>
2024-10-19 16:53:15 +0200

    Update yggdrasil/src/localization/mod.rs
    Co-authored-by: Gijs de Jong <14833076+oxkitsune@users.noreply.github.com>

--- yggdrasil/src/localization/mod.rs
+++ yggdrasil/src/localization/mod.rs
@@ -50,7 +50,7 @@ impl RobotPose {
     #[must_use]
     pub fn as_3d(&self) -> Isometry3<f32> {
         Isometry3::from_parts(
-            Translation3::new(self.inner.translation.x, self.inner.translation.y, 0.5),
+            Translation3::new(self.inner.translation.x, self.inner.translation.y, 0.0),
             UnitQuaternion::from_euler_angles(0.0, 0.0, self.inner.rotation.angle()),
         )
     }
