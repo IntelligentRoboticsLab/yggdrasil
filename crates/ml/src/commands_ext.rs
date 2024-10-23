9ae5fac423217ed0c4adfa86da88dc1b0389e57a
Gijs de Jong <berichtaangijs@gmail.com>
2024-10-23 12:43:16 +0200

    fix rebase

--- crates/ml/src/commands_ext.rs
+++ crates/ml/src/commands_ext.rs
@@ -151,14 +151,6 @@ where
             .request_infer(self.state.0)
             .expect("failed to request inference");
 
-        self.commands.prepare_task(TaskPool::Compute).scope({
-            move |s| {
-                s.spawn(async move {
-                    let output = request
-                        .run()
-                        .map(InferRequest::fetch_output)
-                        .expect("failed to fetch output");
-
         // TODO: This should really be:
         // - On the AsyncCompute pool
         // - Be on it's own thread that blocks (?) (e.g. `blocking` crate)
