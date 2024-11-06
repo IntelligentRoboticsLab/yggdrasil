2701f66e712738f042161cffa734273016729c74
Harold Ruiter <harold@soaratorium.com>
2024-11-06 20:46:59 +0100

    Initial commit

---
+++ crates/filter/examples/ukf_pose.rs
@@ -0,0 +1,186 @@
+use filter::{StateVec, UkfState, UnscentedKalmanFilter};
+
+use plotters::prelude::*;
+use rand::Rng;
+
+use nalgebra::{self as na, Complex, ComplexField, SVector, UnitComplex, Vector3};
+
+const UPDATE_INTERVAL: usize = 5;
+
+#[derive(Debug, Clone)]
+struct Pose {
+    isom: na::Isometry3<f32>,
+}
+
+impl Pose {
+    fn new(x: f32, y: f32, theta: f32) -> Self {
+        Self {
+            isom: na::Isometry3::from_parts(
+                na::Translation3::from(na::Vector3::new(x, y, 0.0)),
+                na::UnitQuaternion::from_axis_angle(&Vector3::z_axis(), theta),
+            ),
+        }
+    }
+
+    fn apply_motion(&self, offset: &Self) -> Self {
+        Self {
+            isom: offset.isom * self.isom,
+        }
+    }
+}
+
+impl UkfState<3> for Pose {
+    fn into_weighted_mean<T>(iter: T) -> StateVec<3>
+    where
+        T: Iterator<Item = (f32, Self)>,
+    {
+        let mut mean_translation = SVector::zeros();
+        let mut mean_angle = Complex::new(0.0, 0.0);
+
+        for (weight, pose) in iter {
+            let translation = pose.isom.translation.vector;
+            let rotation = pose.isom.rotation.angle();
+
+            mean_translation += weight * translation;
+            mean_angle += weight * Complex::cis(rotation);
+        }
+
+        mean_translation.xy().push(mean_angle.argument())
+    }
+
+    fn center(self, mean: &StateVec<3>) -> StateVec<3> {
+        let translation = self.isom.translation.vector;
+        let rotation = self.isom.rotation.angle();
+
+        (translation.xy() - mean.xy())
+            .push((UnitComplex::new(rotation) / UnitComplex::new(mean.z)).angle())
+    }
+}
+
+impl From<Pose> for StateVec<3> {
+    fn from(pose: Pose) -> Self {
+        let translation = pose.isom.translation.vector;
+        let rotation = pose.isom.rotation;
+        translation.xy().push(rotation.angle())
+    }
+}
+
+impl From<StateVec<3>> for Pose {
+    fn from(state: StateVec<3>) -> Self {
+        Self {
+            isom: na::Isometry3::from_parts(
+                na::Translation3::from(state.xy().push(0.0)),
+                na::UnitQuaternion::from_axis_angle(
+                    &na::Unit::new_normalize(na::Vector3::z()),
+                    state.z,
+                ),
+            ),
+        }
+    }
+}
+
+fn main() {
+    let pose = Pose::new(1.0, 2.0, 0.0);
+    let cov = na::SMatrix::<f32, 3, 3>::identity() * 0.05;
+
+    let mut ukf = UnscentedKalmanFilter::<3, 7, Pose>::new(pose, cov);
+
+    // generate 100 noisy measurements
+    let mut rng = rand::thread_rng();
+    let mut x_true = vec![];
+    let mut x_noisy = vec![];
+    let mut x_unscented = vec![];
+
+    for i in 0..150 {
+        let prev = x_true
+            .last()
+            .cloned()
+            .unwrap_or_else(|| Pose::new(1.0, 2.0, 0.0));
+
+        let offset = Pose::new(-0.05, 0.1, -0.02).apply_motion(&Pose::new(
+            rng.gen_range(-0.001..0.001),
+            rng.gen_range(-0.01..0.01),
+            rng.gen_range(-0.01..0.01),
+        ));
+
+        let measurement = prev.apply_motion(&offset);
+
+        x_true.push(measurement.clone());
+
+        let noisy_offset = offset.apply_motion(&Pose::new(
+            rng.gen_range(-1.0..1.0) * cov[(0, 0)],
+            rng.gen_range(-1.0..1.0) * cov[(1, 1)],
+            rng.gen_range(-1.0..1.0) * cov[(2, 2)],
+        ));
+
+        let noisy_prev = x_noisy
+            .last()
+            .cloned()
+            .unwrap_or_else(|| Pose::new(1.0, 2.0, 0.0));
+
+        let noisy = noisy_prev.apply_motion(&noisy_offset);
+
+        x_noisy.push(noisy);
+
+        ukf.predict(
+            |p| p.apply_motion(&noisy_offset),
+            filter::CovMat::identity(),
+        )
+        .unwrap();
+
+        // Every nth step, update the filter with a measurement
+        //
+        // Uses a very low covariance as we are very sure about our measurements
+        if i % UPDATE_INTERVAL == 0 {
+            ukf.update(|p| p, measurement, filter::CovMat::identity() * 0.001)
+                .unwrap();
+        }
+
+        x_unscented.push(ukf.state().clone());
+    }
+
+    // plot the results
+    let root_area = BitMapBackend::new("./ukf_pose.png", (600, 400)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
+
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .margin_top(20)
+        .margin_right(20)
+        .build_cartesian_2d(0.0..10.0f32, 0.0..10.0f32)
+        .unwrap();
+
+    ctx.configure_mesh().draw().unwrap();
+
+    ctx.draw_series(x_true.iter().map(|point| {
+        let translation = point.isom.translation.vector;
+        Circle::new((translation.x, translation.y), 2, RED.filled())
+    }))
+    .unwrap()
+    .label("Ground truth")
+    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    ctx.draw_series(x_noisy.iter().map(|point| {
+        let translation = point.isom.translation.vector;
+        Circle::new((translation.x, translation.y), 2, BLUE.filled())
+    }))
+    .unwrap()
+    .label("Dead reckoning")
+    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
+
+    ctx.draw_series(x_unscented.iter().map(|point| {
+        let translation = point.isom.translation.vector;
+        Circle::new((translation.x, translation.y), 2, GREEN.filled())
+    }))
+    .unwrap()
+    .label("Ukf values")
+    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &GREEN));
+
+    ctx.configure_series_labels()
+        .border_style(&BLACK)
+        .background_style(&WHITE.mix(0.8))
+        .position(SeriesLabelPosition::UpperLeft)
+        .draw()
+        .unwrap();
+}
---
+++ crates/filter/src/lib.rs
@@ -0,0 +1,277 @@
+use std::{fmt::Debug, marker::PhantomData};
+
+use nalgebra::{Cholesky, SMatrix, SVector};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Covariance matrix is not definite-positive")]
+    Cholesky,
+    #[error("Matrix is not invertible")]
+    Inversion,
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub type StateVec<const D: usize> = SVector<f32, D>;
+pub type CovMat<const D: usize> = SMatrix<f32, D, D>;
+pub type CrossCovMat<const D1: usize, const D2: usize> = SMatrix<f32, D1, D2>;
+
+pub type SigmaPoints1 = SigmaPoints<1, 3>;
+pub type SigmaPoints2 = SigmaPoints<2, 5>;
+pub type SigmaPoints3 = SigmaPoints<3, 7>;
+pub type SigmaPoints4 = SigmaPoints<4, 9>;
+
+/// Merwe scaled sigma points
+///
+/// N should be `2 * D + 1` where D is the dimension of your state vector
+#[derive(Debug)]
+pub struct SigmaPoints<const D_STATE: usize, const N_SIGMAS: usize> {
+    pub alpha: f32,
+    pub beta: f32,
+    pub kappa: f32,
+    /// weights for means and covariances
+    pub w_m: SVector<f32, N_SIGMAS>,
+    pub w_c: SVector<f32, N_SIGMAS>,
+}
+
+impl<const D_STATE: usize, const N_SIGMAS: usize> SigmaPoints<D_STATE, N_SIGMAS> {
+    /// A typical recommendation is alpha = 1, beta = 0, and kappa ≈ 3D / 2.
+    ///
+    /// If the true distribution is Gaussian, beta = 2 is optimal.
+    #[must_use]
+    pub fn new(alpha: f32, beta: f32, kappa: f32) -> Self {
+        // TODO: if const generic arithmetic stabilises we can remove the N_SIGMAS generic parameter.
+        assert_eq!(2 * D_STATE + 1, N_SIGMAS);
+
+        let (w_m, w_c) = Self::calculate_weights(alpha, beta, kappa);
+
+        Self {
+            alpha,
+            beta,
+            kappa,
+            w_m,
+            w_c,
+        }
+    }
+
+    fn calculate_weights(
+        alpha: f32,
+        beta: f32,
+        kappa: f32,
+    ) -> (SVector<f32, N_SIGMAS>, SVector<f32, N_SIGMAS>) {
+        let d = D_STATE as f32;
+
+        let a_squared_k = alpha.powi(2) * kappa;
+
+        let w = 1.0 / (2.0 * a_squared_k);
+        let mut w_m = SVector::<f32, N_SIGMAS>::repeat(w);
+        let mut w_c = SVector::<f32, N_SIGMAS>::repeat(w);
+
+        w_m[0] = (a_squared_k - d) / a_squared_k;
+        w_c[0] = w_m[0] + 1.0 - alpha.powi(2) + beta;
+
+        (w_m, w_c)
+    }
+
+    fn calculate(
+        &self,
+        mean: &StateVec<D_STATE>,
+        covariance: &CovMat<D_STATE>,
+    ) -> Result<SMatrix<f32, N_SIGMAS, D_STATE>> {
+        // get the lower triangular matrix from cholesky decomposition
+        let cholesky_l = Cholesky::new(*covariance).ok_or(Error::Cholesky)?.l();
+
+        let mut sigma_points = SMatrix::<f32, N_SIGMAS, D_STATE>::zeros();
+
+        // s_0 = mean
+        sigma_points.set_row(0, &mean.transpose());
+
+        for i in 0..D_STATE {
+            let u = self.alpha * self.kappa.sqrt() * cholesky_l.column(i);
+            // s_1, ..., s_n = mean + alpha * sqrt(kappa) * l.T_i
+            sigma_points.set_row(i + 1, &(mean + u).transpose());
+            // s_n+1, ..., s_2n = mean - alpha * sqrt(kappa) * l.T_i
+            sigma_points.set_row(i + 1 + D_STATE, &(mean - u).transpose());
+        }
+
+        Ok(sigma_points)
+    }
+}
+
+pub struct UnscentedKalmanFilter<
+    const D_STATE: usize,
+    const N_SIGMAS: usize,
+    Motion: UkfState<D_STATE>,
+> {
+    sigmas: SigmaPoints<D_STATE, N_SIGMAS>,
+    state: StateVec<D_STATE>,
+    covariance: CovMat<D_STATE>,
+    _measurement: PhantomData<Motion>,
+}
+
+impl<const D_STATE: usize, const N_SIGMAS: usize, Motion: UkfState<D_STATE>>
+    UnscentedKalmanFilter<D_STATE, N_SIGMAS, Motion>
+{
+    #[must_use]
+    pub fn new<S, C>(state_0: S, covariance_0: C) -> Self
+    where
+        S: Into<StateVec<D_STATE>>,
+        C: Into<CovMat<D_STATE>>,
+    {
+        Self::with_sigma_points(
+            SigmaPoints::new(1.0, 0.0, D_STATE as f32 * 3.0 / 2.0),
+            state_0,
+            covariance_0,
+        )
+    }
+
+    #[must_use]
+    pub fn with_sigma_points<S, C>(
+        sigmas: SigmaPoints<D_STATE, N_SIGMAS>,
+        state_0: S,
+        covariance_0: C,
+    ) -> Self
+    where
+        S: Into<StateVec<D_STATE>>,
+        C: Into<CovMat<D_STATE>>,
+    {
+        Self {
+            sigmas,
+            state: state_0.into(),
+            covariance: covariance_0.into(),
+            _measurement: PhantomData,
+        }
+    }
+
+    #[must_use]
+    pub fn state(&self) -> Motion {
+        self.state.into()
+    }
+
+    #[must_use]
+    pub fn covariance(&self) -> CovMat<D_STATE> {
+        self.covariance
+    }
+
+    pub fn predict<F>(
+        &mut self,
+        transition_function: F,
+        transition_noise: CovMat<D_STATE>,
+    ) -> Result<()>
+    where
+        F: Fn(Motion) -> Motion,
+    {
+        let sigma_points = self.sigmas.calculate(&self.state, &self.covariance)?;
+
+        // apply the motion model to each sigma point
+        let transformed_sigma_points = sigma_points
+            .row_iter()
+            .map(|row| transition_function(row.transpose().into()));
+
+        // calculate the new state mean and covariance
+        let mean = UkfState::into_weighted_mean(
+            self.sigmas
+                .w_m
+                .iter()
+                .copied()
+                .zip(transformed_sigma_points.clone()),
+        );
+
+        let covariance: CovMat<D_STATE> = {
+            // start with additive process noise
+            let mut covariance = transition_noise;
+
+            for (i, point) in transformed_sigma_points.enumerate() {
+                let centered = point.center(&mean);
+                covariance += self.sigmas.w_c[i] * centered * centered.transpose();
+            }
+
+            covariance
+        };
+
+        self.state = mean;
+        self.covariance = covariance;
+
+        Ok(())
+    }
+
+    pub fn update<const D_MEASUREMENT: usize, Measurement, F>(
+        &mut self,
+        measurement_function: F,
+        measurement: Measurement,
+        measurement_noise: CovMat<D_MEASUREMENT>,
+    ) -> Result<()>
+    where
+        Measurement: UkfState<D_MEASUREMENT>,
+        F: Fn(Motion) -> Measurement,
+    {
+        let sigma_points_matrix = self.sigmas.calculate(&self.state, &self.covariance)?;
+
+        let sigma_points = sigma_points_matrix
+            .row_iter()
+            .map(|row| row.transpose().into());
+
+        // apply the measurement model to each sigma point
+        #[allow(clippy::redundant_closure)]
+        let transformed_sigma_points = sigma_points.clone().map(|p| measurement_function(p));
+
+        // calculate the new state mean and covariance
+        let mean = UkfState::into_weighted_mean(
+            self.sigmas
+                .w_m
+                .iter()
+                .copied()
+                .zip(transformed_sigma_points.clone()),
+        );
+
+        let covariance: CovMat<D_MEASUREMENT> = {
+            // start with additive measurement noise
+            let mut covariance = measurement_noise;
+
+            for (i, point) in transformed_sigma_points.clone().enumerate() {
+                let centered = point.center(&mean);
+                covariance += self.sigmas.w_c[i] * centered * centered.transpose();
+            }
+
+            covariance
+        };
+
+        let cross_covariance: CrossCovMat<D_STATE, D_MEASUREMENT> = {
+            let mut cross_covariance = CrossCovMat::<D_STATE, D_MEASUREMENT>::zeros();
+
+            for (i, (transformed_sigma_point, sigma_point)) in
+                transformed_sigma_points.zip(sigma_points).enumerate()
+            {
+                // we need to center the measurement
+                let measurement_centered = transformed_sigma_point.center(&mean);
+
+                // and also our predicted current motion state
+                let motion_centered = sigma_point.center(&self.state);
+
+                cross_covariance +=
+                    self.sigmas.w_c[i] * motion_centered * measurement_centered.transpose();
+            }
+
+            cross_covariance
+        };
+
+        let kalman_gain = cross_covariance * covariance.try_inverse().ok_or(Error::Inversion)?;
+        let innovation = measurement.center(&mean);
+
+        self.state += kalman_gain * innovation;
+        self.covariance -= kalman_gain * covariance * kalman_gain.transpose();
+
+        Ok(())
+    }
+}
+
+pub trait UkfState<const D: usize>: From<StateVec<D>> + Sized {
+    /// Calculates the mean state from an iterator over weights and points
+    fn into_weighted_mean<T>(iter: T) -> StateVec<D>
+    where
+        T: Iterator<Item = (f32, Self)>;
+
+    /// Centers `Self` around a mean state
+    fn center(self, mean: &StateVec<D>) -> StateVec<D>;
+}
