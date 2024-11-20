2bfe9585414448cc643daaffd79130a46b010af0
Aemulation <112875887+Aemulation@users.noreply.github.com>
2024-11-20 14:01:25 +0100

    Add the thighs and tibias to the body contour

--- yggdrasil/src/vision/body_contour/mod.rs
+++ yggdrasil/src/vision/body_contour/mod.rs
@@ -1,16 +1,11 @@
 use std::ops::Deref;
 
-use bevy::{prelude::*, reflect::List};
+use bevy::prelude::*;
 use heimdall::{Bottom, CameraLocation, CameraMatrix};
 use nalgebra::{Isometry3, Point2, Vector3};
 
 use crate::{
-    core::debug::DebugContext,
-    kinematics::{
-        spaces::{Chest, LeftShoulderCap, LeftToe, RightShoulderCap, RightToe, Robot},
-        Kinematics,
-    },
-    nao::Cycle,
+    core::debug::DebugContext, kinematics::prelude::*, nao::Cycle,
     sensor::orientation::RobotOrientation,
 };
 
@@ -25,6 +20,12 @@ pub struct BodyContour {
     right_toe_point: Option<Point2<f32>>,
 
     chest_point: Option<Point2<f32>>,
+
+    left_thigh_point: Option<Point2<f32>>,
+    right_thigh_point: Option<Point2<f32>>,
+
+    left_tibia_point: Option<Point2<f32>>,
+    right_tibia_point: Option<Point2<f32>>,
 }
 
 impl BodyContour {
@@ -37,6 +38,18 @@ impl BodyContour {
         }) || self
             .chest_point
             .is_some_and(|chest_point| Self::is_part_of_chest(chest_point, image_coordinate))
+            || self
+                .left_thigh_point
+                .is_some_and(|thigh_point| Self::is_part_of_thigh(thigh_point, image_coordinate))
+            || self
+                .right_thigh_point
+                .is_some_and(|thigh_point| Self::is_part_of_thigh(thigh_point, image_coordinate))
+            || self
+                .left_tibia_point
+                .is_some_and(|tibia_point| Self::is_part_of_tibia(tibia_point, image_coordinate))
+            || self
+                .right_tibia_point
+                .is_some_and(|tibia_point| Self::is_part_of_tibia(tibia_point, image_coordinate))
     }
 
     fn is_part_of_shoulder(shoulder_point: Point2<f32>, image_coordinate: Point2<f32>) -> bool {
@@ -51,6 +64,22 @@ impl BodyContour {
             && chest_point.x + 40.0 > image_coordinate.x
             && chest_point.y - 20.0 < image_coordinate.y
             && chest_point.y + 40.0 > image_coordinate.y
+    }
+
+    // # TODO: This might be too simple for a body part that's not static.
+    fn is_part_of_thigh(thigh_point: Point2<f32>, image_coordinate: Point2<f32>) -> bool {
+        thigh_point.x - 20.0 < image_coordinate.x
+            && thigh_point.x + 20.0 > image_coordinate.x
+            && thigh_point.y - 20.0 < image_coordinate.y
+            && thigh_point.y + 20.0 > image_coordinate.y
+    }
+
+    // # TODO: This might be too simple for a body part that's not static.
+    fn is_part_of_tibia(tibia_point: Point2<f32>, image_coordinate: Point2<f32>) -> bool {
+        tibia_point.x - 20.0 < image_coordinate.x
+            && tibia_point.x + 20.0 > image_coordinate.x
+            && tibia_point.y - 20.0 < image_coordinate.y
+            && tibia_point.y + 20.0 > image_coordinate.y
     }
 
     fn update_toes(
@@ -123,6 +152,58 @@ impl BodyContour {
             )
             .ok();
     }
+
+    fn update_thighs(
+        &mut self,
+        orientation: &RobotOrientation,
+        kinematics: &Kinematics,
+        matrix: &CameraMatrix<Bottom>,
+    ) {
+        let (robot_to_left_thigh, robot_to_right_thigh) = robot_to_thighs(orientation, kinematics);
+
+        self.left_thigh_point = matrix
+            .ground_to_pixel(
+                (robot_to_left_thigh.inverse() * matrix.robot_to_ground)
+                    .translation
+                    .vector
+                    .into(),
+            )
+            .ok();
+        self.right_thigh_point = matrix
+            .ground_to_pixel(
+                (robot_to_right_thigh.inverse() * matrix.robot_to_ground)
+                    .translation
+                    .vector
+                    .into(),
+            )
+            .ok();
+    }
+
+    fn update_tibias(
+        &mut self,
+        orientation: &RobotOrientation,
+        kinematics: &Kinematics,
+        matrix: &CameraMatrix<Bottom>,
+    ) {
+        let (robot_to_left_tibia, robot_to_right_tibia) = robot_to_tibias(orientation, kinematics);
+
+        self.left_tibia_point = matrix
+            .ground_to_pixel(
+                (robot_to_left_tibia.inverse() * matrix.robot_to_ground)
+                    .translation
+                    .vector
+                    .into(),
+            )
+            .ok();
+        self.right_tibia_point = matrix
+            .ground_to_pixel(
+                (robot_to_right_tibia.inverse() * matrix.robot_to_ground)
+                    .translation
+                    .vector
+                    .into(),
+            )
+            .ok();
+    }
 }
 
 #[derive(Default)]
@@ -153,20 +234,22 @@ fn update_body_contours(
     bottom_image: Res<Image<Bottom>>,
     current_cycle: Res<Cycle>,
 ) {
-    // if bottom_image.is_from_cycle(*current_cycle) {
-    //     body_contour.update_toes(&orientation, &kinematics,
-    // &bottom_camera_matrix);
-    //
-    //     body_contour.update_chest(&orientation, &kinematics,
-    // &bottom_camera_matrix);
-    //
-    //     body_contour.update_shoulders(&orientation, &kinematics,
-    // &bottom_camera_matrix); }
+    if bottom_image.is_from_cycle(*current_cycle) {
+        body_contour.update_toes(&orientation, &kinematics, &bottom_camera_matrix);
+        body_contour.update_chest(&orientation, &kinematics, &bottom_camera_matrix);
+        body_contour.update_shoulders(&orientation, &kinematics, &bottom_camera_matrix);
+        body_contour.update_thighs(&orientation, &kinematics, &bottom_camera_matrix);
+        body_contour.update_tibias(&orientation, &kinematics, &bottom_camera_matrix);
+    }
 
     // #TODO: for debugging, remove.
     body_contour.chest_point = Some(Point2::new(200.0, 200.0));
     body_contour.left_shoulder_cap_point = Some(Point2::new(160.0, 140.0));
     body_contour.right_shoulder_cap_point = Some(Point2::new(240.0, 140.0));
+    body_contour.left_thigh_point = Some(Point2::new(160.0, 240.0));
+    body_contour.right_thigh_point = Some(Point2::new(240.0, 240.0));
+    body_contour.left_tibia_point = Some(Point2::new(180.0, 300.0));
+    body_contour.right_tibia_point = Some(Point2::new(220.0, 300.0));
 }
 
 fn visualize_body_contour(
@@ -245,5 +328,49 @@ fn robot_to_shoulders(
     (
         imu_adjusted_robot_to_left_shoulder_cap,
         imu_adjusted_robot_to_right_shoulder_cap,
+    )
+}
+
+fn robot_to_thighs(
+    orientation: &RobotOrientation,
+    kinematics: &Kinematics,
+) -> (Isometry3<f32>, Isometry3<f32>) {
+    let (roll, pitch, _) = orientation.euler_angles();
+
+    let robot_to_left_thigh = kinematics.isometry::<Robot, LeftThigh>().inner;
+    let imu_adjusted_robot_to_left_thigh = Isometry3::from(robot_to_left_thigh.translation)
+        * Isometry3::rotation(Vector3::y() * pitch)
+        * Isometry3::rotation(Vector3::x() * roll);
+
+    let robot_to_right_thigh = kinematics.isometry::<Robot, RightThigh>().inner;
+    let imu_adjusted_robot_to_right_thigh = Isometry3::from(robot_to_right_thigh.translation)
+        * Isometry3::rotation(Vector3::y() * pitch)
+        * Isometry3::rotation(Vector3::x() * roll);
+
+    (
+        imu_adjusted_robot_to_left_thigh,
+        imu_adjusted_robot_to_right_thigh,
+    )
+}
+
+fn robot_to_tibias(
+    orientation: &RobotOrientation,
+    kinematics: &Kinematics,
+) -> (Isometry3<f32>, Isometry3<f32>) {
+    let (roll, pitch, _) = orientation.euler_angles();
+
+    let robot_to_left_tibia = kinematics.isometry::<Robot, LeftTibia>().inner;
+    let imu_adjusted_robot_to_left_tibia = Isometry3::from(robot_to_left_tibia.translation)
+        * Isometry3::rotation(Vector3::y() * pitch)
+        * Isometry3::rotation(Vector3::x() * roll);
+
+    let robot_to_right_tibia = kinematics.isometry::<Robot, RightTibia>().inner;
+    let imu_adjusted_robot_to_right_tibia = Isometry3::from(robot_to_right_tibia.translation)
+        * Isometry3::rotation(Vector3::y() * pitch)
+        * Isometry3::rotation(Vector3::x() * roll);
+
+    (
+        imu_adjusted_robot_to_left_tibia,
+        imu_adjusted_robot_to_right_tibia,
     )
 }
