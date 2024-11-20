145947ea5ada82c41ada78da5ffa78ba40400316
Aemulation <112875887+Aemulation@users.noreply.github.com>
2024-11-20 13:31:51 +0100

    Working body contour

--- yggdrasil/src/vision/body_contour/mod.rs
+++ yggdrasil/src/vision/body_contour/mod.rs
@@ -1,6 +1,6 @@
 use std::ops::Deref;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, reflect::List};
 use heimdall::{Bottom, CameraLocation, CameraMatrix};
 use nalgebra::{Isometry3, Point2, Vector3};
 
@@ -28,6 +28,31 @@ pub struct BodyContour {
 }
 
 impl BodyContour {
+    #[must_use]
+    pub fn is_part_of_body(&self, image_coordinate: Point2<f32>) -> bool {
+        self.left_shoulder_cap_point.is_some_and(|shoulder_point| {
+            Self::is_part_of_shoulder(shoulder_point, image_coordinate)
+        }) || self.right_shoulder_cap_point.is_some_and(|shoulder_point| {
+            Self::is_part_of_shoulder(shoulder_point, image_coordinate)
+        }) || self
+            .chest_point
+            .is_some_and(|chest_point| Self::is_part_of_chest(chest_point, image_coordinate))
+    }
+
+    fn is_part_of_shoulder(shoulder_point: Point2<f32>, image_coordinate: Point2<f32>) -> bool {
+        shoulder_point.x - 20.0 < image_coordinate.x
+            && shoulder_point.x + 20.0 > image_coordinate.x
+            && shoulder_point.y - 20.0 < image_coordinate.y
+            && shoulder_point.y + 20.0 > image_coordinate.y
+    }
+
+    fn is_part_of_chest(chest_point: Point2<f32>, image_coordinate: Point2<f32>) -> bool {
+        chest_point.x - 40.0 < image_coordinate.x
+            && chest_point.x + 40.0 > image_coordinate.x
+            && chest_point.y - 20.0 < image_coordinate.y
+            && chest_point.y + 40.0 > image_coordinate.y
+    }
+
     fn update_toes(
         &mut self,
         orientation: &RobotOrientation,
@@ -107,39 +132,16 @@ impl Plugin for BodyContourPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<BodyContour>()
             .add_systems(PostStartup, setup_body_contour_visualization::<Bottom>)
-            .add_systems(Update, update_body_contours);
-        // .add_systems(Update, print_toes::<Bottom>)
-        // .add_systems(Update, print_chest::<Bottom>)
-        // .add_systems(Update, print_shoulders::<Bottom>);
+            .add_systems(Update, update_body_contours)
+            .add_systems(Update, visualize_body_contour.after(update_body_contours));
     }
 }
 
 fn setup_body_contour_visualization<T: CameraLocation>(dbg: DebugContext) {
     dbg.log_component_batches(
-        T::make_entity_path("body_contour/toes"),
+        T::make_entity_path("body_contour"),
         true,
-        [
-            &rerun::Color::from_rgb(219, 62, 177) as _,
-            &rerun::Radius::new_ui_points(14.0) as _,
-        ],
-    );
-
-    dbg.log_component_batches(
-        T::make_entity_path("body_contour/chest"),
-        true,
-        [
-            &rerun::Color::from_rgb(255, 255, 0) as _,
-            &rerun::Radius::new_ui_points(14.0) as _,
-        ],
-    );
-
-    dbg.log_component_batches(
-        T::make_entity_path("body_contour/shoulders"),
-        true,
-        [
-            &rerun::Color::from_rgb(0, 238, 255) as _,
-            &rerun::Radius::new_ui_points(14.0) as _,
-        ],
+        [&rerun::Color::from_rgb(219, 62, 177) as _],
     );
 }
 
@@ -151,122 +153,42 @@ fn update_body_contours(
     bottom_image: Res<Image<Bottom>>,
     current_cycle: Res<Cycle>,
 ) {
-    if bottom_image.is_from_cycle(*current_cycle) {
-        body_contour.update_toes(&orientation, &kinematics, &bottom_camera_matrix);
+    // if bottom_image.is_from_cycle(*current_cycle) {
+    //     body_contour.update_toes(&orientation, &kinematics,
+    // &bottom_camera_matrix);
+    //
+    //     body_contour.update_chest(&orientation, &kinematics,
+    // &bottom_camera_matrix);
+    //
+    //     body_contour.update_shoulders(&orientation, &kinematics,
+    // &bottom_camera_matrix); }
 
-        body_contour.update_chest(&orientation, &kinematics, &bottom_camera_matrix);
-
-        body_contour.update_shoulders(&orientation, &kinematics, &bottom_camera_matrix);
-    }
+    // #TODO: for debugging, remove.
+    body_contour.chest_point = Some(Point2::new(200.0, 200.0));
+    body_contour.left_shoulder_cap_point = Some(Point2::new(160.0, 140.0));
+    body_contour.right_shoulder_cap_point = Some(Point2::new(240.0, 140.0));
 }
 
-fn print_toes<T: CameraLocation>(
-    orientation: Res<RobotOrientation>,
-    kinematics: Res<Kinematics>,
+fn visualize_body_contour(
+    body_contour: Res<BodyContour>,
     debug_context: DebugContext,
-    matrix: Res<CameraMatrix<T>>,
-    bottom_image: Res<Image<T>>,
-    current_cycle: Res<Cycle>,
+    bottom_image: Res<Image<Bottom>>,
 ) {
-    if !bottom_image.is_from_cycle(*current_cycle) {
-        return;
+    let mut points = Vec::with_capacity(480 * 720);
+
+    for x in 0..bottom_image.yuyv_image().width() {
+        for y in 0..bottom_image.yuyv_image().height() {
+            let x = x as f32;
+            let y = y as f32;
+            if body_contour.is_part_of_body(Point2::new(x, y)) {
+                points.push((x, y));
+            }
+        }
     }
 
-    let (robot_to_left_toe, robot_to_right_toe) = robot_to_toes(&orientation, &kinematics);
-
-    let (Ok(left_toe_point), Ok(right_toe_point)) = (
-        matrix.ground_to_pixel(
-            (robot_to_left_toe.inverse() * matrix.robot_to_ground)
-                .translation
-                .vector
-                .into(),
-        ),
-        matrix.ground_to_pixel(
-            (robot_to_right_toe.inverse() * matrix.robot_to_ground)
-                .translation
-                .vector
-                .into(),
-        ),
-    ) else {
-        return;
-    };
-
     debug_context.log_with_cycle(
-        T::make_entity_path("body_contour/toes"),
+        Bottom::make_entity_path("body_contour"),
         bottom_image.deref().cycle(),
-        &rerun::Points2D::new([
-            (left_toe_point.x, left_toe_point.y),
-            (right_toe_point.x, right_toe_point.y),
-        ]),
-    );
-}
-
-fn print_chest<T: CameraLocation>(
-    orientation: Res<RobotOrientation>,
-    kinematics: Res<Kinematics>,
-    debug_context: DebugContext,
-    matrix: Res<CameraMatrix<T>>,
-    bottom_image: Res<Image<T>>,
-    current_cycle: Res<Cycle>,
-) {
-    if !bottom_image.is_from_cycle(*current_cycle) {
-        return;
-    }
-
-    let robot_to_chest = robot_to_chest(&orientation, &kinematics);
-
-    let Ok(chest_point) = matrix.ground_to_pixel(
-        (robot_to_chest.inverse() * matrix.robot_to_ground)
-            .translation
-            .vector
-            .into(),
-    ) else {
-        return;
-    };
-
-    debug_context.log_with_cycle(
-        T::make_entity_path("body_contour/chest"),
-        *current_cycle,
-        &rerun::Points2D::new([(chest_point.x, chest_point.y)]),
-    );
-}
-
-fn print_shoulders<T: CameraLocation>(
-    orientation: Res<RobotOrientation>,
-    kinematics: Res<Kinematics>,
-    debug_context: DebugContext,
-    matrix: Res<CameraMatrix<T>>,
-    bottom_image: Res<Image<T>>,
-    current_cycle: Res<Cycle>,
-) {
-    if !bottom_image.is_from_cycle(*current_cycle) {
-        return;
-    }
-
-    let (robot_to_left_shoulder_cap, robot_to_right_shoulder_cap) =
-        robot_to_shoulders(&orientation, &kinematics);
-
-    let mut points = Vec::new();
-    if let Ok(left_shoulder_cap_point) = matrix.ground_to_pixel(
-        (robot_to_left_shoulder_cap.inverse() * matrix.robot_to_ground)
-            .translation
-            .vector
-            .into(),
-    ) {
-        points.push((left_shoulder_cap_point.x, left_shoulder_cap_point.y));
-    }
-    if let Ok(right_shoulder_cap_point) = matrix.ground_to_pixel(
-        (robot_to_right_shoulder_cap.inverse() * matrix.robot_to_ground)
-            .translation
-            .vector
-            .into(),
-    ) {
-        points.push((right_shoulder_cap_point.x, right_shoulder_cap_point.y));
-    }
-
-    debug_context.log_with_cycle(
-        T::make_entity_path("body_contour/shoulders"),
-        *current_cycle,
         &rerun::Points2D::new(&points),
     );
 }
