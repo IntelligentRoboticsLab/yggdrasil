3416cf220ec2fc930f331f5d488c7f3d5ef3dc1f
Julia Blaauboer <j.s.blaauboer@gmail.com>
2024-11-23 19:11:40 +0100

    x macro fuckery

--- yggdrasil/src/kinematics/visualization.rs
+++ yggdrasil/src/kinematics/visualization.rs
@@ -5,6 +5,37 @@ use super::prelude::*;
 
 pub struct KinematicsVisualizationPlugin;
 
+macro_rules! meshes {
+    ($($x:tt)*) => {
+        macro_rules! x {$($x)*}
+        x! {
+            head, Head
+            neck, Neck
+            robot, Robot
+            left_shoulder, LeftShoulder
+            left_upper_arm, LeftUpperArm
+            left_forearm, LeftForearm
+            left_wrist, LeftWrist
+            left_pelvis, LeftPelvis
+            left_hip, LeftHip
+            left_thigh, LeftThigh
+            left_tibia, LeftTibia
+            left_ankle, LeftAnkle
+            left_foot, LeftFoot
+            right_shoulder, RightShoulder
+            right_upper_arm, RightUpperArm
+            right_forearm, RightForearm
+            right_wrist, RightWrist
+            right_pelvis, RightPelvis
+            right_hip, RightHip
+            right_thigh, RightThigh
+            right_tibia, RightTibia
+            right_ankle, RightAnkle
+            right_foot, RightFoot
+        }
+    };
+}
+
 impl Plugin for KinematicsVisualizationPlugin {
     fn build(&self, app: &mut App) {
         app
@@ -13,102 +44,74 @@ impl Plugin for KinematicsVisualizationPlugin {
     }
 }
 
-macro_rules! log_meshes {
-    {$($name:ident: $space:ty,)*} => {
-        #[derive(Default)]
-        struct TransformBuffer {
-            cycle: Vec<i64>,
-            $($name: (Vec<rerun::components::Translation3D>, Vec<rerun::components::RotationQuat>),)*
-        }
+meshes!{($($name:ident, $_:ty)*) => {
+    #[derive(Default)]
+    struct TransformBuffer {
+        cycle: Vec<i64>,
+        $($name: (Vec<rerun::components::Translation3D>, Vec<rerun::components::RotationQuat>),)*
+    }
+}}
 
-        fn setup_meshes(dbg: DebugContext) {
-            dbg.log_component_batches(
-                "nao",
-                true,
-                [&rerun::components::AxisLength(rerun::Float32(0.)) as _],
-            );
+fn setup_meshes(dbg: DebugContext) {
+    dbg.log_component_batches(
+        "nao",
+        true,
+        [&rerun::components::AxisLength(rerun::Float32(0.)) as _],
+    );
 
-            $(
-                let path = concat!("nao/", stringify!($name));
+    meshes!{($($name:ident, $_:ty)*) => {$(
+        let path = concat!("nao/", stringify!($name));
 
-                dbg.log_static(
-                    path,
-                    &rerun::Asset3D::from_file(concat!("./assets/rerun/nao/", stringify!($name), ".glb"))
-                        .expect(concat!("Failed to load ", stringify!($), " model"))
-                        .with_media_type(rerun::MediaType::glb()),
-                );
+        dbg.log_static(
+            path,
+            &rerun::Asset3D::from_file(concat!("./assets/rerun/nao/", stringify!($name), ".glb"))
+                .expect(concat!("Failed to load ", stringify!($), " model"))
+                .with_media_type(rerun::MediaType::glb()),
+        );
 
-                dbg.log_static(path, &rerun::ViewCoordinates::FLU);
-            )*
-        }
-
-        fn update_meshes(
-            dbg: DebugContext,
-            kinematics: Res<Kinematics>,
-            pose: Res<RobotPose>,
-            cycle: Res<Cycle>,
-            mut buffer: Local<TransformBuffer>,
-        ) {
-            let pose = pose.as_3d();
-
-            dbg.log(
-                "nao",
-                &rerun::Transform3D::from_translation_rotation(
-                    pose.translation.vector.data.0[0],
-                    rerun::Quaternion(pose.rotation.coords.data.0[0]),
-                ),
-            );
-
-            let robot_to_ground = kinematics.robot_to_ground();
-
-            buffer.cycle.push(cycle.0 as i64);
-
-            $(
-                let isometry = kinematics.isometry::<$space, _>().chain(robot_to_ground.as_ref());
-
-                buffer.$name.0.push(isometry.inner.translation.vector.data.0[0].into());
-                buffer.$name.1.push(isometry.inner.rotation.coords.data.0[0].into());
-            )*
-
-            if buffer.cycle.len() >= 50 {
-                let timeline = rerun::TimeColumn::new_sequence("cycle", std::mem::take(&mut buffer.cycle));
-
-                $(
-                    dbg.send_columns(concat!("nao/", stringify!($name)), [timeline.clone()], [
-                        &buffer.$name.0 as _,
-                        &buffer.$name.1 as _,
-                    ]);
-
-                    buffer.$name.0.clear();
-                    buffer.$name.1.clear();
-                )*
-            }
-        }
-    };
+        dbg.log_static(path, &rerun::ViewCoordinates::FLU);
+    )*}}
 }
 
-log_meshes! {
-   	head: Head,
-	neck: Neck,
-	robot: Robot,
-	left_shoulder: LeftShoulder,
-	left_upper_arm: LeftUpperArm,
-	left_forearm: LeftForearm,
-	left_wrist: LeftWrist,
-	left_pelvis: LeftPelvis,
-	left_hip: LeftHip,
-	left_thigh: LeftThigh,
-	left_tibia: LeftTibia,
-	left_ankle: LeftAnkle,
-	left_foot: LeftFoot,
-	right_shoulder: RightShoulder,
-	right_upper_arm: RightUpperArm,
-	right_forearm: RightForearm,
-	right_wrist: RightWrist,
-	right_pelvis: RightPelvis,
-	right_hip: RightHip,
-	right_thigh: RightThigh,
-	right_tibia: RightTibia,
-	right_ankle: RightAnkle,
-	right_foot: RightFoot,
+fn update_meshes(
+    dbg: DebugContext,
+    kinematics: Res<Kinematics>,
+    pose: Res<RobotPose>,
+    cycle: Res<Cycle>,
+    mut buffer: Local<TransformBuffer>,
+) {
+    let pose = pose.as_3d();
+
+    dbg.log(
+        "nao",
+        &rerun::Transform3D::from_translation_rotation(
+            pose.translation.vector.data.0[0],
+            rerun::Quaternion(pose.rotation.coords.data.0[0]),
+        ),
+    );
+
+    let robot_to_ground = kinematics.robot_to_ground();
+
+    buffer.cycle.push(cycle.0 as i64);
+
+    meshes!{($($name:ident, $space:ty)*) => {$(
+        let isometry = kinematics.isometry::<$space, _>().chain(robot_to_ground.as_ref());
+
+        buffer.$name.0.push(isometry.inner.translation.vector.data.0[0].into());
+        buffer.$name.1.push(isometry.inner.rotation.coords.data.0[0].into());
+    )*}}
+
+    if buffer.cycle.len() >= 50 {
+        let timeline = rerun::TimeColumn::new_sequence("cycle", std::mem::take(&mut buffer.cycle));
+
+        meshes!{($($name:ident, $_space:ty)*) => {$(
+            dbg.send_columns(concat!("nao/", stringify!($name)), [timeline.clone()], [
+                &buffer.$name.0 as _,
+                &buffer.$name.1 as _,
+            ]);
+
+            buffer.$name.0.clear();
+            buffer.$name.1.clear();
+        )*}}
+    }
 }
