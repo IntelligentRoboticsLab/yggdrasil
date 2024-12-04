a29b048316315d48a8132fd39ce9a8c7d6ad23cd
Julia Blaauboer <44556294+juliablbr@users.noreply.github.com>
2024-12-04 18:24:16 +0100

    Add goal and ball meshes to Rerun (#410)

--- yggdrasil/src/vision/ball_detection/mod.rs
+++ yggdrasil/src/vision/ball_detection/mod.rs
@@ -15,7 +15,7 @@ use serde_with::{serde_as, DurationMilliSeconds};
 
 use crate::{
     core::debug::DebugContext,
-    nao::{NaoManager, Priority},
+    nao::{Cycle, NaoManager, Priority},
     prelude::*,
 };
 
@@ -38,9 +38,11 @@ impl Plugin for BallDetectionPlugin {
                 init_subconfigs,
                 setup_ball_debug_logging::<Top>,
                 setup_ball_debug_logging::<Bottom>,
+                setup_3d_ball_debug_logging,
             ),
         )
-        .add_systems(Update, detected_ball_eye_color);
+        .add_systems(Update, detected_ball_eye_color)
+        .add_systems(PostUpdate, log_3d_balls);
     }
 }
 
@@ -109,5 +111,55 @@ fn detected_ball_eye_color(
         }
     } else {
         nao.set_left_eye_led(LeftEye::fill(color::f32::EMPTY), Priority::default());
+    }
+}
+
+fn setup_3d_ball_debug_logging(dbg: DebugContext) {
+    dbg.log_static(
+        "balls/best",
+        &rerun::Asset3D::from_file("./assets/rerun/ball.glb")
+            .expect("failed to load ball model")
+            .with_media_type(rerun::MediaType::glb()),
+    );
+
+    dbg.log_static("balls/best", &rerun::ViewCoordinates::FLU);
+    dbg.log_with_cycle(
+        "balls/best",
+        Cycle::default(),
+        &rerun::Transform3D::from_scale((0., 0., 0.)),
+    );
+}
+
+fn log_3d_balls(
+    dbg: DebugContext,
+    top_balls: Res<Balls<Top>>,
+    bottom_balls: Res<Balls<Bottom>>,
+    mut last_logged: Local<Option<Cycle>>,
+) {
+    let most_confident_ball = bottom_balls
+        .most_confident_ball()
+        .map(|b| (b.cycle, b.position))
+        .or(top_balls
+            .most_confident_ball()
+            .map(|b| (b.cycle, b.position)));
+
+    if let Some((cycle, pos)) = most_confident_ball {
+        // since we always run this function in the same cycle in which the ball was found,
+        // using `log_with_cycle` would be redundant.
+        //
+        // because `last_logged` starts off at 0, we wouldn't log the ball if it was detected in
+        // the very first cycle, i don't really care tho, deal with it.
+        if last_logged.map_or(true, |c| cycle > c) {
+            *last_logged = Some(cycle);
+            dbg.log(
+                "balls/best",
+                &rerun::Transform3D::from_translation((pos.coords.x, pos.coords.y, 0.05)),
+            );
+        }
+    } else if last_logged.is_some() {
+        // this feels very hacky but i was told this is the most idiomatic way to hide stuff in
+        // rerun.
+        *last_logged = None;
+        dbg.log("balls/best", &rerun::Transform3D::from_scale((0., 0., 0.)));
     }
 }
