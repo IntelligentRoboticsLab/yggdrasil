27a9053ab6e0a350526dd55a0418ffb54606c79f
John Yao <yaojohn6@gmail.com>
2025-01-27 11:00:51 +0100

    format

--- tools/sindri/src/cli/robot_ops.rs
+++ tools/sindri/src/cli/robot_ops.rs
@@ -589,7 +589,12 @@ pub(crate) async fn stop_single_yggdrasil_service(robot: &Robot, output: Output)
 /// Copy the contents of the 'deploy' folder to the robot.
 pub(crate) async fn upload_to_robot(addr: &Ipv4Addr) -> Result<()> {
     let output = Command::new("rsync")
-        .args(["-az", "deploy/", &format!("nao@{addr}:/home/nao"), "--out-format=\"%f\""])
+        .args([
+            "-az",
+            "deploy/",
+            &format!("nao@{addr}:/home/nao"),
+            "--out-format=\"%f\"",
+        ])
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .output()
