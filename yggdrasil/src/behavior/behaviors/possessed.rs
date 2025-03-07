fdfdf73ff2d76dfbc94251ab05d9f7e4c1230c03
Julia Blaauboer <j.s.blaauboer@gmail.com>
2025-03-07 19:03:30 +0100

    death

--- yggdrasil/src/behavior/behaviors/possessed.rs
+++ yggdrasil/src/behavior/behaviors/possessed.rs
@@ -16,6 +16,8 @@ impl Plugin for PossessedBehaviorPlugin {
 const POSSESSED_WALK_SPEED: f32 = 0.045;
 const POSSESSED_SIDE_SPEED: f32 = 0.045;
 const POSSESSED_TURN_SPEED: f32 = 0.2;
+const POSSESSED_LEFT_DEADBAND: f32 = 0.1;
+const POSSESSED_RIGHT_DEADBAND: f32 = 0.1;
 
 #[derive(Resource)]
 pub struct Possessed;
@@ -38,11 +40,23 @@ pub fn possessed(
         }
     }
 
+    let left_stick = if gamepad.left_stick().length() > POSSESSED_LEFT_DEADBAND {
+        gamepad.left_stick()
+    } else {
+        Vec2::ZERO
+    };
+
+    let right_stick = if gamepad.right_stick().length() > POSSESSED_RIGHT_DEADBAND {
+        Vec2::Y.angle_to(gamepad.right_stick())
+    } else {
+        0.
+    };
+
     if !matches!(**gait, Gait::Sitting) {
         step_context.request_walk(Step {
-            forward: POSSESSED_WALK_SPEED * gamepad.left_stick().y,
-            left: -POSSESSED_SIDE_SPEED * gamepad.left_stick().x,
-            turn: POSSESSED_TURN_SPEED * gamepad.right_stick().angle_to(Vec2::Y),
+            forward: POSSESSED_WALK_SPEED * left_stick.y,
+            left: -POSSESSED_SIDE_SPEED * left_stick.x,
+            turn: POSSESSED_TURN_SPEED * right_stick,
         });
     }
 }
