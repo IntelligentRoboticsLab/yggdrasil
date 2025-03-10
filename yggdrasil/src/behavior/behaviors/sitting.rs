f5661292c1618d86bd251c372991faac5211ae46
Aemulation <112875887+Aemulation@users.noreply.github.com>
2025-03-10 16:18:53 +0100

    Unstiff if sitting for > 5 secs

--- yggdrasil/src/behavior/behaviors/sitting.rs
+++ yggdrasil/src/behavior/behaviors/sitting.rs
@@ -1,18 +1,23 @@
 use bevy::prelude::*;
 
 use nidhogg::types::{color, FillExt, RightEye};
+use std::time::{Duration, Instant};
 
 use crate::{
     behavior::engine::{in_behavior, Behavior, BehaviorState},
-    motion::walking_engine::step_context::StepContext,
+    motion::walking_engine::{step_context::StepContext, Gait},
     nao::{NaoManager, Priority},
 };
+
+const MIN_SITTING_DURATION_BEFORE_UNSTIFF: Duration = Duration::from_secs(5);
 
 pub struct SittingBehaviorPlugin;
 
 impl Plugin for SittingBehaviorPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, sitting.run_if(in_behavior::<Sitting>));
+        app.add_systems(Update, sitting.run_if(in_behavior::<Sitting>))
+            .add_systems(OnEnter(Gait::Sitting), reset_sitting_instant)
+            .insert_resource(SittingInstant(Instant::now()));
     }
 }
 
@@ -25,8 +30,29 @@ impl Behavior for Sitting {
     const STATE: BehaviorState = BehaviorState::Sitting;
 }
 
-pub fn sitting(mut step_context: ResMut<StepContext>, mut nao_manager: ResMut<NaoManager>) {
-    // Makes right eye blue.
+#[derive(Resource)]
+pub struct SittingInstant(Instant);
+
+fn reset_sitting_instant(mut time_since_sitting: ResMut<SittingInstant>) {
+    time_since_sitting.0 = Instant::now();
+}
+
+pub fn sitting(
+    mut step_context: ResMut<StepContext>,
+    mut nao_manager: ResMut<NaoManager>,
+    gait: Res<State<Gait>>,
+    time_since_sitting: Res<SittingInstant>,
+) {
     nao_manager.set_right_eye_led(RightEye::fill(color::f32::BLUE), Priority::default());
-    step_context.request_sit();
+
+    match &gait.get() {
+        Gait::Sitting => {
+            if time_since_sitting.0.elapsed() > MIN_SITTING_DURATION_BEFORE_UNSTIFF {
+                nao_manager.unstiff_sit(Priority::High);
+            }
+        }
+        _ => {
+            step_context.request_sit();
+        }
+    }
 }
