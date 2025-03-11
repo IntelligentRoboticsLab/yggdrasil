98de255f973d78deca5ffafcced02c1a73c43765
Fyor Klein Gunnewiek <fyor.klein.gunnewiek@volvocars.com>
2025-03-11 13:16:53 +0100

    more

--- yggdrasil/src/behavior/roles/kick_in.rs
+++ yggdrasil/src/behavior/roles/kick_in.rs
@@ -10,6 +10,7 @@ use crate::{
 };
 
 const GIJS_HAS_KICK_WORKING: bool = false;
+const DISTANCE_TO_SETPOSITION: f32 = 0.4;
 
 /// Plugin for the KickIn role
 pub struct KickInRolePlugin;
@@ -43,12 +44,29 @@ pub fn kick_walk_role(
     layout_config: Res<LayoutConfig>,
     step_planner: ResMut<StepPlanner>,
     behavior: Res<State<BehaviorState>>,
+   
     game_controller_message: Option<Res<GameControllerMessage>>,
 ) {
+    let set_robot_position = layout_config
+        .set_positions
+        .player(player_config.player_number);
+
+    let set_position = set_robot_position.isometry.translation.vector;
+
+    // Check distance to the set position
+    let set_point = Point2::new(set_position.x, set_position.y);
+    let close_to_target = pose.distance_to(&set_point) < DISTANCE_TO_SETPOSITION;
+
+    // Check if the robot it is facing it's own side of the field. If in front of him the x is going negative
+    let aligned_with_rotation = (pose.world_rotation() - set_robot_position.isometry.rotation.angle()).abs() < 0.2;
+
+    // Check if the robot sees a ball and is close to it
+    let ball_distance = pose.distance_to(&pose);
+
     if let Some(game_state) = game_controller_message.as_deref() {
-        let penalized_robots = game_controller_message.
+        let penalized_robots = game_state.is_penalized(4, player_config.team_number);
 
     }
 
-    if player_config.player_number == 
+
 }
