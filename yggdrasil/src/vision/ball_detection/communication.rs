c4b011e37d53e1dfc854c856875c10db5e6e73e3
Fiona <fiona.nagelhout@gmail.com>
2025-03-14 12:40:16 +0100

    fix errors

--- yggdrasil/src/vision/ball_detection/communication.rs
+++ yggdrasil/src/vision/ball_detection/communication.rs
@@ -1,4 +1,6 @@
-use bevy::ecs::system::ResMut;
+use std::net::SocketAddr;
+
+use bevy::{ecs::system::ResMut, utils::HashMap};
 use nalgebra as na;
 
 use crate::communication::{TeamCommunication, TeamMessage};
@@ -28,33 +30,33 @@ impl CommunicatedBalls {
     }
 
     /// Send your ball position (even if it's None) as a message.
-    fn send_message(&mut self, ball_position: Option<na::Point2<f32>>) {
+    fn send_message(&mut self, ball_position: Option<na::Point2<f32>>){
         tc.outbound_mut()
-            .update_or_push(TeamMessage::Ball(ball_position));
+            .update_or_push(TeamMessage::DetectedBall(ball_position));
         self.sent = ball_position;
     }
 
     /// Receive messages.
     // 2.A.a. If no other robot are detecting a ball, we return the same None we had
     // 2.A.b. If there are other robots detecting a ball, we take one from theirs as our own.
-    fn receive_messages(comun: &mut TeamCommunication) {
+    fn receive_messages(comms: &mut TeamCommunication) -> Option<na::Point2<f32>{
         let mut received_ball = None;
         while let Some((_, who, ball)) = comms.inbound_mut().take_map(|_, _, what| match what {
             TeamMessage::DetectedBall(ball) => Some(ball),
             _ => None,
         }) {
-            received_ball = received_ball.or(ball);
+            received_ball = received_ball.or(Some(ball));
         }
-        received_ball
+        return received_ball
     }
 
-    fn communicate_balls(
+    fn communicate_balls(&mut self,
         mut tc: ResMut<TeamCommunication>,
         mut most_confident_ball: Option<na::Point2<f32>>,
-    ) {
+    ) -> Option<na::Point2<f32>>{
         // 1. Check if it has changed enough and if so, we send a message.
         let has_changed =
-            CommunicatedBalls::changed_enough(most_confident_ball, comunicated_balls.sent);
+            self.change_enough(&most_confident_ball);
         if has_changed {
             self.send_message(most_confident_ball)
         }
@@ -62,10 +64,10 @@ impl CommunicatedBalls {
         // 2. Receive messages only if our current ball is None.
         // 2.A. If its None we check the received messages.
         if most_confident_ball.is_none() {
-            let ball = receive_messages(TeamCommunication);
+            let ball = CommunicatedBalls::receive_messages(TeamCommunication);
         // 2.B. If our current ball is not None, then we just use our ball.
         } else {
-            ball = most_confident_ball
+            let ball = most_confident_ball;
         }
 
         // 3. We return the ball we are using
