bcd9bb27a979e2610c4da2954b178615db4bb072
Harold Ruiter <harold@soaratorium.com>
2025-03-14 20:58:54 +0100

    fix

--- yggdrasil/src/localization/mod.rs
+++ yggdrasil/src/localization/mod.rs
@@ -13,19 +13,11 @@ pub struct LocalizationPlugin;
 
 impl Plugin for LocalizationPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(
-            (
-                LineCorrespondencePlugin::<Top>::default(),
-                // Disabled for now as we kept seeing lines inside of the robot
-                // LineCorrespondencePlugin::<Bottom>::default(),
-                PoseFilterPlugin,
-            )
-                .add_systems(PostStartup, (init_pose, setup_pose_visualization))
-                .add_systems(
-                    PreUpdate,
-                    update_robot_pose.after(odometry::update_odometry),
-                )
-                .add_systems(PostUpdate, visualize_pose),
-        );
+        app.add_plugins((
+            LineCorrespondencePlugin::<Top>::default(),
+            // Disabled for now as we kept seeing lines inside of the robot
+            // LineCorrespondencePlugin::<Bottom>::default(),
+            PoseFilterPlugin,
+        ));
     }
 }
