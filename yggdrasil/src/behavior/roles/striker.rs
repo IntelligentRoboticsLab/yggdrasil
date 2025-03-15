6781b0c39eea77b8b2d5700fe28430e449366aa0
Fyor Klein Gunnewiek <fyorkg@gmail.com>
2025-03-15 11:37:52 +0100

    Striker

--- yggdrasil/src/behavior/roles/striker.rs
+++ yggdrasil/src/behavior/roles/striker.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use nalgebra::{Point2, Point3};
+use nidhogg::types::{color, FillExt, RightEye};
 
 use crate::{
     behavior::{
@@ -9,12 +10,13 @@ use crate::{
     core::config::layout::LayoutConfig,
     localization::RobotPose,
     motion::{step_planner::Target, walking_engine::step::Step},
+    nao::{NaoManager, Priority},
     vision::ball_detection::{ball_tracker::BallTracker, Hypothesis},
 };
 
 // Walk to the ball as long as the ball is further away than
 // `BALL_DISTANCE_WALK_THRESHOLD` meters.
-const BALL_DISTANCE_WALK_THRESHOLD: f32 = 0.5;
+const BALL_DISTANCE_WALK_THRESHOLD: f32 = 0.75;
 
 /// Plugin for the Striker role
 pub struct StrikerRolePlugin;
@@ -26,13 +28,8 @@ impl Plugin for StrikerRolePlugin {
 }
 
 /// Substates for the `Striker` role
-#[derive(Resource, Default)]
-pub enum Striker {
-    #[default]
-    WalkToBall,
-    WalkAlign,
-    WalkWithBall,
-}
+#[derive(Resource, Default, Debug)]
+pub struct Striker;
 
 impl Roles for Striker {
     const STATE: RoleState = RoleState::Striker;
@@ -44,6 +41,7 @@ pub fn striker_role(
     layout_config: Res<LayoutConfig>,
     ball_tracker: Res<BallTracker>,
     mut state: ResMut<Striker>,
+    mut nao_manager: ResMut<NaoManager>,
 ) {
     if let Hypothesis::Stationary(_) = ball_tracker.cutoff() {
         let ball = ball_tracker.state().0;
@@ -58,11 +56,11 @@ pub fn striker_role(
         let ball_angle = pose.angle_to(&ball);
         let absolute_ball_angle = ball_angle + pose.world_rotation();
 
-        let ball_aligned = ball_angle.abs() < 0.2;
-        let ball_goal_aligned = absolute_ball_angle < absolute_goal_angle_left
-            && absolute_ball_angle > absolute_goal_angle_right;
+        let enemy_goal_left = Point2::new(layout_config.field.length / 2., 0.8);
+        let enemy_goal_right = Point2::new(layout_config.field.length / 2., -0.8);
 
-        let ball_goal_center_align = (absolute_ball_angle - absolute_goal_angle).abs() < 0.2;
+        let relative_goal_left = pose.world_to_robot(&enemy_goal_left);
+        let relative_goal_right = pose.world_to_robot(&enemy_goal_right);
 
         let ball_distance = pose.distance_to(&ball);
 
@@ -71,41 +69,89 @@ pub fn striker_role(
             rotation: None,
         };
 
-        state.next_state(
-            ball_distance,
-            ball_goal_center_align,
-            ball_aligned,
-            ball_goal_aligned,
-        );
+        let goal_aligned = goal_aligned(pose.as_ref(), layout_config.as_ref());
 
-        match *state {
-            Striker::WalkToBall | Striker::WalkWithBall => {
-                commands.set_behavior(WalkTo { target: ball_pos });
-            }
-            Striker::WalkAlign => {
-                let ball_target = Point3::new(ball.x, ball.y, 0.0);
+        let relative_ball = pose.world_to_robot(&ball);
 
-                if absolute_ball_angle > absolute_goal_angle_left {
+        let ball_target = Point3::new(ball.x, ball.y, 0.1);
+        if ball_distance > 0.3 {
+            println!("WalkingToBall");
+            nao_manager.set_right_eye_led(RightEye::fill(color::f32::YELLOW), Priority::default());
+
+            commands.set_behavior(WalkTo { target: ball_pos });
+        } else {
+            if !goal_aligned {
+                println!("Aligning with goal");
+
+                nao_manager
+                    .set_right_eye_led(RightEye::fill(color::f32::ORANGE), Priority::default());
+                if relative_goal_left.y < 0. && relative_goal_right.y < 0. {
                     commands.set_behavior(Walk {
                         step: Step {
+                            forward: 0.00,
                             left: 0.06,
                             turn: -0.3,
-                            ..Default::default()
                         },
                         look_target: Some(ball_target),
                     });
                     return;
                 }
-                if absolute_ball_angle < absolute_goal_angle_right {
+                if relative_goal_left.y > 0. && relative_goal_right.y > 0. {
                     commands.set_behavior(Walk {
                         step: Step {
+                            forward: 0.00,
                             left: -0.06,
                             turn: 0.3,
-                            ..Default::default()
+                        },
+                        look_target: Some(ball_target),
+                    });
+                    return;
+                } else {
+                    println!("WATAAFAAAAK!?!?!?!");
+                }
+            } else if ball_angle.abs() > 0.3 {
+                println!("Aligning with ball");
+
+                nao_manager
+                    .set_right_eye_led(RightEye::fill(color::f32::PURPLE), Priority::default());
+                let ball_target = Point3::new(ball.x, ball.y, 0.1);
+                if relative_ball.y < 0. {
+                    // step right
+                    commands.set_behavior(Walk {
+                        step: Step {
+                            forward: 0.00,
+                            left: -0.04,
+                            turn: 0.0,
+                        },
+                        look_target: Some(ball_target),
+                    });
+                } else {
+                    // step left
+                    commands.set_behavior(Walk {
+                        step: Step {
+                            forward: 0.00,
+                            left: 0.04,
+                            turn: 0.0,
                         },
                         look_target: Some(ball_target),
                     });
                 }
+                println!("We Were goal algined, but not ball aligned!!!!");
+
+                // walk to align with ball
+            } else {
+                println!("Walking with ball");
+
+                nao_manager.set_right_eye_led(RightEye::fill(color::f32::RED), Priority::default());
+
+                commands.set_behavior(Walk {
+                    step: Step {
+                        forward: 0.06,
+                        left: 0.00,
+                        turn: 0.0,
+                    },
+                    look_target: Some(ball_target),
+                });
             }
         }
     } else {
@@ -113,20 +159,49 @@ pub fn striker_role(
     }
 }
 
-impl Striker {
-    fn next_state(
-        &mut self,
-        ball_distance: f32,
-        ball_goal_center_align: bool,
-        ball_aligned: bool,
-        ball_goal_aligned: bool,
-    ) {
-        *self = match self {
-            _ if ball_distance > BALL_DISTANCE_WALK_THRESHOLD => Striker::WalkToBall,
-            Striker::WalkToBall if ball_distance < 0.3 => Striker::WalkAlign,
-            Striker::WalkAlign if ball_goal_center_align && ball_aligned => Striker::WalkWithBall,
-            Striker::WalkWithBall if !ball_goal_aligned => Striker::WalkAlign,
-            _ => return,
-        }
+pub fn goal_aligned(pose: &RobotPose, layout_config: &LayoutConfig) -> bool {
+    if pose.inner.translation.x > 0.0 {
+        // If on enemy side
+        is_aligned_with_goal(pose, layout_config)
+    } else {
+        // If on own side
+        is_aligned_with_enemyside(pose, layout_config)
+    }
+}
+
+pub fn is_aligned_with_goal(pose: &RobotPose, layout_config: &LayoutConfig) -> bool {
+    // returns true if we are angled inbetween the goal posts
+
+    let enemy_goal_left = Point2::new(layout_config.field.length / 2., 0.8);
+    let enemy_goal_right = Point2::new(layout_config.field.length / 2., -0.8);
+
+    let relative_goal_left = pose.world_to_robot(&enemy_goal_left);
+    let relative_goal_right = pose.world_to_robot(&enemy_goal_right);
+
+    if relative_goal_left.y > 0. && relative_goal_right.y < 0. {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// returns true if we are angled inbetween the two corners of the enemy side
+pub fn is_aligned_with_enemyside(pose: &RobotPose, layout_config: &LayoutConfig) -> bool {
+    let enemy_goal_left = Point2::new(
+        layout_config.field.length / 2.,
+        layout_config.field.width / 2.,
+    );
+    let enemy_goal_right = Point2::new(
+        layout_config.field.length / 2.,
+        -layout_config.field.width / 2.,
+    );
+
+    let relative_goal_left = pose.world_to_robot(&enemy_goal_left);
+    let relative_goal_right = pose.world_to_robot(&enemy_goal_right);
+
+    if relative_goal_left.y > 0. && relative_goal_right.y < 0. {
+        return true;
+    } else {
+        return false;
     }
 }
