25b43110ab8ed80fcc0169e7a6c5cdf472e4c3d5
Gijs de Jong <berichtaangijs@gmail.com>
2025-04-02 21:38:12 +0200

    energy efficient stand

--- yggdrasil/src/motion/walking_engine/gait/stand.rs
+++ yggdrasil/src/motion/walking_engine/gait/stand.rs
@@ -1,12 +1,18 @@
 use bevy::prelude::*;
-use nidhogg::types::{FillExt, LegJoints};
+use nidhogg::{
+    types::{FillExt, JointArray, LegJoints},
+    NaoState,
+};
 
-use crate::motion::walking_engine::{
-    config::WalkingEngineConfig,
-    feet::FootPositions,
-    hips::HipHeight,
-    schedule::{Gait, WalkingEngineSet},
-    TargetFootPositions, TargetLegStiffness,
+use crate::{
+    motion::walking_engine::{
+        config::WalkingEngineConfig,
+        feet::FootPositions,
+        hips::HipHeight,
+        schedule::{Gait, WalkingEngineSet},
+        TargetFootPositions, TargetLegStiffness,
+    },
+    nao::CycleTime,
 };
 
 pub(super) struct StandGaitPlugin;
@@ -33,4 +39,38 @@ fn generate_stand_gait(
 
     hip_height.request(config.hip_height.walking_hip_height);
     **target_stiffness = LegJoints::fill(config.walking_leg_stiffness);
+}
+
+const MIN_CURRENT: f32 = 0.09;
+const MAX_CURRENT: f32 = 0.12;
+const REDUCTION: f32 = 0.000005;
+
+/// System that optimises the requested joint positions based on the maximum current.
+///
+/// Based on the implementation as described in the Berlin United 2019 Tech Report.
+fn energy_efficient_stand(
+    state: Res<NaoState>,
+    mut minimum_reached: Local<bool>,
+    mut joint_offsets: Local<JointArray<f32>>,
+    cycle_time: Res<CycleTime>,
+) {
+    let (joint_idx, max_current) = state
+        .current
+        .into_iter()
+        .enumerate()
+        .max_by(|(_, a), (_, b)| a.total_cmp(b))
+        .expect("e");
+
+    *minimum_reached = if *max_current < MIN_CURRENT {
+        true
+    } else if *max_current > MAX_CURRENT {
+        false
+    } else {
+        *minimum_reached
+    };
+
+    if !*minimum_reached {
+        let max_adjustment = REDUCTION / cycle_time.duration.as_secs_f32();
+        // TODO(gijsd): apply to joint at index `joint_idx`
+    }
 }
