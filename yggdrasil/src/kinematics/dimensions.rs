ad04bb00fa5fd220d006d9a6b91c130e33c8f1ad
Aemulation <112875887+Aemulation@users.noreply.github.com>
2025-04-07 12:52:56 +0200

    Fix chest dimension

--- yggdrasil/src/kinematics/dimensions.rs
+++ yggdrasil/src/kinematics/dimensions.rs
@@ -33,7 +33,7 @@ pub const NECK_TO_TOP_CAMERA: Vector3<f32> = Vector3::new(0.05871, 0.0, 0.06364)
 /// Vector pointing from the neck to the bottom camera.
 pub const NECK_TO_BOTTOM_CAMERA: Vector3<f32> = Vector3::new(0.05071, 0.0, 0.01774);
 
-pub const TORSO_TO_CHEST: Vector3<f32> = Vector3::new(0.10, 0.0, 0.09);
+pub const TORSO_TO_CHEST: Vector3<f32> = Vector3::new(0.01, 0.0, 0.09);
 pub const CHEST_TO_CHEST_LEFT: Vector3<f32> = Vector3::new(0.0, 0.07, 0.0);
 pub const CHEST_TO_CHEST_RIGHT: Vector3<f32> = Vector3::new(0.0, -0.07, 0.0);
 pub const ROBOT_TO_LEFT_SHOULDER_CAP: Vector3<f32> = Vector3::new(-0.02, 0.098, 0.22);
