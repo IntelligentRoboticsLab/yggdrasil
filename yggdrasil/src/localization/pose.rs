184aa501d4940f7891501325d6331ecfeeab0981
Gijs de Jong <berichtaangijs@gmail.com>
2025-04-08 17:40:45 +0200

    Slightly lower camera height

--- yggdrasil/src/localization/pose.rs
+++ yggdrasil/src/localization/pose.rs
@@ -18,7 +18,7 @@ pub struct RobotPose {
 impl RobotPose {
     // Constant for camera height that we set anywhere get_lookat_absolute is called.
     // Set to zero if we are only looking at the ground, for example.
-    pub const CAMERA_HEIGHT: f32 = 0.5;
+    pub const CAMERA_HEIGHT: f32 = 0.35;
 
     #[must_use]
     pub fn from_isometry(pose: Isometry2<f32>) -> Self {
