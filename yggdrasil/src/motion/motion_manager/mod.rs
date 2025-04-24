01eb457b02c09c7458cfa43809ce700690c6679c
Aemulation <112875887+Aemulation@users.noreply.github.com>
2025-04-24 21:33:55 +0200

    WIP: motion executor

--- yggdrasil/src/motion/motion_manager/mod.rs
+++ yggdrasil/src/motion/motion_manager/mod.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use bevy::prelude::*;
 use odal::Config;
 use serde::{Deserialize, Serialize};
@@ -9,8 +11,93 @@ pub struct MotionManagerPlugin;
 
 impl Plugin for MotionManagerPlugin {
     fn build(&self, app: &mut App) {
-        app.init_config::<GetUpBackMotionConfig>();
+        app.init_config::<GetUpBackMotionConfig>()
+            .init_resource::<MotionManager>()
+            .add_systems(Update, load_next_motion);
     }
+}
+
+fn load_next_motion(
+    mut motion_manager: ResMut<MotionManager>,
+    getup_back_motion_config: Res<GetUpBackMotionConfig>,
+) {
+    let Some(next_motion) = &motion_manager.next_motion else {
+        return;
+    };
+
+    match next_motion {
+        Motion::GetUpBack => {
+            motion_manager.current_motions = Some((0, getup_back_motion_config.motions.clone()));
+            motion_manager.next_motion = None;
+        }
+    }
+}
+
+fn run_motion(
+    mut motion_manager: ResMut<MotionManager>,
+    imu_values: Res<IMUValues>,
+    completed_motion_at: Local<Option<Instant>>,
+) {
+    let Some((current_motion_id, motions)) = motion_manager.current_motions.as_ref() else {
+        return;
+    };
+
+    let current_motion = &motions[*current_motion_id];
+
+    // Check joing angles for complete motion.
+    //  else return
+
+    // If angles are correct, set `completed_motion_at` if unset.
+
+    // Check `end_conditions`,
+    //  if (end_conditions.all() == true && completed_motion_at.elapsed > min_delay) ||
+    //  completed_motion_at.elapsed > max_delay {
+    //
+    //  } else {
+    //      continue;
+    //  }
+
+    // Set next motion.
+}
+
+#[derive(Default, Resource)]
+pub struct MotionManager {
+    current_motions: Option<(usize, Vec<Motions>)>,
+    next_motion: Option<Motion>,
+}
+
+impl MotionManager {
+    pub fn set_motion_if_unset(&mut self, motion: Motion) {
+        self.next_motion.get_or_insert(motion);
+    }
+
+    pub fn overwrite_motion(&mut self, motion: Motion) {
+        self.next_motion = Some(motion);
+    }
+
+    fn current_motion<'a>(&'a self) -> Option<&'a Motions> {
+        self.current_motions
+            .iter()
+            .flat_map(|(current_motion_id, motions)| motions.get(*current_motion_id))
+            .next()
+    }
+
+    fn next_motion(&mut self) {
+        let Some((current_motion_id, motions)) = self.current_motions.as_mut() else {
+            return;
+        };
+
+        *current_motion_id += 1;
+        if *current_motion_id == motions.len() {
+            *current_motion_id = 0;
+            motions.clear();
+        }
+        return;
+    }
+}
+
+pub enum Motion {
+    GetUpBack,
 }
 
 // #[derive(Serialize, Deserialize, Debug, Clone)]
