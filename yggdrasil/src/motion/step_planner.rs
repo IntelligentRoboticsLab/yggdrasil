af4f67ff582d0b8ee543798eaa1d6a1ce88dc68d
VerkuijlKim <verkuijlkim@gmail.com>
2025-05-07 17:17:35 +0200

    update

--- yggdrasil/src/motion/step_planner.rs
+++ yggdrasil/src/motion/step_planner.rs
@@ -5,7 +5,7 @@ use super::{
 use crate::{core::debug::DebugContext, localization::RobotPose, nao::Cycle};
 use bevy::prelude::*;
 use nalgebra::{Isometry, Point2, UnitComplex, Vector2};
-use rerun::LineStrip3D;
+use rerun::{FillMode, LineStrip3D};
 use std::time::Instant;
 
 const TURN_SPEED: f32 = 0.2;
@@ -89,6 +89,7 @@ impl StepPlanner {
     }
 
     pub fn add_dynamic_obstacle(&mut self, obstacle: DynamicObstacle, merge_distance: f32) {
+        println!("dynamic obstacle added");
         match self
             .dynamic_obstacles
             .iter_mut()
@@ -100,6 +101,7 @@ impl StepPlanner {
     }
 
     fn collect_and_gc_dynamic_obstacles(&mut self) -> Vec<Obstacle> {
+        println!("collect is called");
         let now = Instant::now();
 
         self.dynamic_obstacles.retain(|obs| now < obs.ttl);
@@ -163,6 +165,7 @@ impl StepPlanner {
     }
 
     pub fn plan(&mut self, robot_pose: &RobotPose) -> Option<Step> {
+        println!("call plan");
         let target = self.target?;
         let (path, _total_walking_distance) = self.calc_path(robot_pose)?;
 
@@ -271,24 +274,21 @@ fn log_dynamic_obstacles(dbg: DebugContext, step_planner: Res<StepPlanner>) {
     let centers = step_planner
         .dynamic_obstacles
         .iter()
-        .map(|obs| [obs.obs.x.0, obs.obs.y.0, 0.0])
+        .map(|obs| (obs.obs.x.0, obs.obs.y.0, 0.0))
         .collect::<Vec<_>>();
 
     let half_sizes = step_planner
         .dynamic_obstacles
         .iter()
-        .map(|obs| [obs.obs.radius.0, obs.obs.radius.0, 0.01])
+        .map(|obs| (obs.obs.radius.0, obs.obs.radius.0, 0.05))
         .collect::<Vec<_>>();
 
     let len = centers.len();
 
     dbg.log(
         "dynamic_obstacle",
-        &rerun::Ellipsoids3D::from_centers_and_half_sizes(centers, half_sizes).with_colors(vec![
-            [
-                255, 0, 0
-            ];
-            len
-        ]),
-    );
+        &rerun::Ellipsoids3D::from_centers_and_half_sizes(centers, half_sizes)
+            .with_colors(vec![[0, 255, 247]; len])
+            .with_fill_mode(FillMode::Solid),
+    )
 }
