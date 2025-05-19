49de75e2c6d10be65c986743ee7a31838eab9217
marinaog <116306804+marinaog@users.noreply.github.com>
2025-05-19 23:08:00 +0200

    merging conflict

--- yggdrasil/src/communication/team.rs
+++ yggdrasil/src/communication/team.rs
@@ -6,6 +6,7 @@ use bevy::prelude::{App, *};
 use miette::IntoDiagnostic;
 use tracing::{debug, warn};
 use nalgebra as na;
+use tracing::{debug, warn};
 
 use crate::core::config::showtime::ShowtimeConfig;
 use crate::prelude::Result;
