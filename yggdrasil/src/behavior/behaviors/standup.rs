81862e7ec6991b14767d0765c68636fe702a523f
Gijs de Jong <berichtaangijs@gmail.com>
2025-07-18 13:00:23 -0300

    Add gyro balancing during get up motion

--- yggdrasil/src/behavior/behaviors/standup.rs
+++ yggdrasil/src/behavior/behaviors/standup.rs
@@ -1,10 +1,17 @@
 use bevy::prelude::*;
+use nalgebra::Vector2;
+use tracing::info;
 
 use crate::{
     behavior::engine::{Behavior, BehaviorState, in_behavior},
     motion::keyframe::{KeyframeExecutor, MotionType},
-    nao::Priority,
-    sensor::falling::{FallState, LyingDirection},
+    nao::{JointSettings, NaoManager, Priority},
+    prelude::PreWrite,
+    sensor::{
+        falling::{FallState, LyingDirection},
+        imu::IMUValues,
+        low_pass_filter::ExponentialLpf,
+    },
 };
 
 /// Behavior dedicated to handling the getup sequence of the robot.
@@ -30,7 +37,15 @@ pub struct StandupBehaviorPlugin;
 
 impl Plugin for StandupBehaviorPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, standup.run_if(in_behavior::<Standup>));
+        app.init_resource::<StandupBalanceAdjustment>();
+        app.add_systems(
+            Update,
+            (update_filtered_gyroscope, standup)
+                .chain()
+                .run_if(in_behavior::<Standup>),
+        );
+
+        app.add_systems(PreWrite, balance_standup.run_if(in_behavior::<Standup>));
     }
 }
 
@@ -53,4 +68,55 @@ fn standup(
 
     // Update completed status based on motion activity
     standup.completed = !keyframe_executor.is_motion_active();
+}
+
+#[derive(Resource)]
+pub struct StandupBalanceAdjustment {
+    pub filtered_gyro: ExponentialLpf<3>,
+}
+
+impl Default for StandupBalanceAdjustment {
+    fn default() -> Self {
+        Self {
+            filtered_gyro: ExponentialLpf::new(0.3),
+        }
+    }
+}
+
+fn update_filtered_gyroscope(
+    mut balance_adjustment: ResMut<StandupBalanceAdjustment>,
+    imu: Res<IMUValues>,
+) {
+    balance_adjustment.filtered_gyro.update(imu.gyroscope);
+}
+
+fn balance_standup(
+    standup: Res<Standup>,
+    mut nao_manager: ResMut<NaoManager>,
+    balance_adjustment: ResMut<StandupBalanceAdjustment>,
+) {
+    if standup.completed {
+        return;
+    }
+
+    let settings = nao_manager.current_legs();
+    let mut joints_position = settings.joints_position.clone();
+    let joints_stiffness = settings.joints_stiffness.clone();
+
+    let gyro = balance_adjustment.filtered_gyro.state();
+
+    let balancing_factor = Vector2::new(0.0, 0.05);
+
+    joints_position.left_leg.ankle_pitch += balancing_factor.y * gyro.y;
+    joints_position.left_leg.ankle_roll += balancing_factor.x * gyro.x;
+    joints_position.left_leg.hip_yaw_pitch += balancing_factor.x * gyro.x;
+    joints_position.right_leg.ankle_pitch += balancing_factor.y * gyro.y;
+    joints_position.right_leg.ankle_roll += balancing_factor.x * gyro.x;
+
+    info!(
+        "overwriting with balance amount: {:.2}",
+        balancing_factor.x * gyro.x
+    );
+
+    nao_manager.set_legs(joints_position, joints_stiffness, Priority::Custom(99));
 }
