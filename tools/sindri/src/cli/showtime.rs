504cb87062cf2bf46df3e4013a1eb8d9cd9aae6d
Aemulation <112875887+Aemulation@users.noreply.github.com>
2025-10-04 11:51:27 +0200

    Use cli team-number if provided

--- tools/sindri/src/cli/showtime.rs
+++ tools/sindri/src/cli/showtime.rs
@@ -24,8 +24,12 @@ impl Showtime {
     /// This command compiles yggdrasil, stops the yggdrasil service on each robot
     /// uploads binaries and other assets and the restarts the yggdrasil service
     /// on each robot.
-    pub async fn showtime(self, config: SindriConfig) -> Result<()> {
+    pub async fn showtime(self, mut config: SindriConfig) -> Result<()> {
         self.robot_ops.prepare_showtime_config(&config)?;
+
+        if let Some(team_number) = self.robot_ops.team {
+            config.team_number = team_number;
+        }
 
         let compile_bar = ProgressBar::new(1);
         let output = robot_ops::Output::Single(compile_bar.clone());
